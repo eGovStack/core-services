ff81d870102ebb4f5cd588d4046ba2734b70e0b0
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-10-04 00:59:02 +0530

    Migrating branch from old repo to new repo

--- egov-user/src/main/java/org/egov/user/web/controller/UserController.java
+++ egov-user/src/main/java/org/egov/user/web/controller/UserController.java
@@ -1,10 +1,9 @@
 package org.egov.user.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.egov.common.contract.response.ResponseInfo;
-import org.egov.user.domain.model.User;
-import org.egov.user.domain.model.UserDetail;
-import org.egov.user.domain.model.UserSearchCriteria;
+import org.egov.user.domain.model.*;
 import org.egov.user.domain.service.TokenService;
 import org.egov.user.domain.service.UserService;
 import org.egov.user.web.contract.*;
@@ -16,7 +15,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,7 +44,7 @@ public class UserController {
 
     @Value("${egov.user.search.default.size}")
     private Integer defaultSearchSize;
-
+	
 
 	@Autowired
 	public UserController(UserService userService, TokenService tokenService) {
@@ -62,10 +65,10 @@ public class UserController {
 		User user = createUserRequest.toDomain(true);
 		user.setOtpValidationMandatory(IsValidationMandatory);
 		if(isRegWithLoginEnabled) {
-			Object object = userService.registerWithLogin(user);
+			Object object = userService.registerWithLogin(user,createUserRequest.getRequestInfo());
 			return new ResponseEntity<>(object, HttpStatus.OK);
 		}
-		User createdUser = userService.createCitizen(user);
+		User createdUser = userService.createCitizen(user,createUserRequest.getRequestInfo());
 		return createResponse(createdUser);
 	}
 
@@ -79,10 +82,11 @@ public class UserController {
 	@PostMapping("/users/_createnovalidate")
 	public UserDetailResponse createUserWithoutValidation(@RequestBody CreateUserRequest createUserRequest,
 			@RequestHeader HttpHeaders headers) {
+
 		User user = createUserRequest.toDomain(true);
 		user.setMobileValidationMandatory(isMobileValidationRequired(headers));
 		user.setOtpValidationMandatory(false);
-		final User newUser = userService.createUser(user);
+		final User newUser = userService.createUser(user,createUserRequest.getRequestInfo());
 		return createResponse(newUser);
 	}
 
@@ -126,6 +130,7 @@ public class UserController {
 	public CustomUserDetails getUser(@RequestParam(value = "access_token") String accessToken) {
 		final UserDetail userDetail = tokenService.getUser(accessToken);
 		return new CustomUserDetails(userDetail);
+		//  no encrypt/decrypt
 	}
 
 	/**
@@ -136,12 +141,12 @@ public class UserController {
 	 * @return
 	 */
 	@PostMapping("/users/_updatenovalidate")
-	public UserDetailResponse updateUserWithoutValidation(@RequestBody final CreateUserRequest createUserRequest,
-														  @RequestHeader HttpHeaders headers) {
+	public UpdateResponse updateUserWithoutValidation(@RequestBody final CreateUserRequest createUserRequest,
+													  @RequestHeader HttpHeaders headers) {
 		User user = createUserRequest.toDomain(false);
 		user.setMobileValidationMandatory(isMobileValidationRequired(headers));
-		final User updatedUser = userService.updateWithoutOtpValidation( user);
-		return createResponse(updatedUser);
+		final User updatedUser = userService.updateWithoutOtpValidation( user,createUserRequest.getRequestInfo());
+		return createResponseforUpdate(updatedUser);
 	}
 
 	/**
@@ -151,17 +156,23 @@ public class UserController {
 	 * @return
 	 */
 	@PostMapping("/profile/_update")
-	public UserDetailResponse patch(@RequestBody final CreateUserRequest createUserRequest) {
+	public UpdateResponse patch(@RequestBody final CreateUserRequest createUserRequest) {
 		log.info("Received Profile Update Request  " + createUserRequest);
 		User user = createUserRequest.toDomain(false);
-		final User updatedUser = userService.partialUpdate(user);
-		return createResponse(updatedUser);
+		final User updatedUser = userService.partialUpdate(user,createUserRequest.getRequestInfo());
+		return createResponseforUpdate(updatedUser);
 	}
 
 	private UserDetailResponse createResponse(User newUser) {
 		UserRequest userRequest = new UserRequest(newUser);
 		ResponseInfo responseInfo = ResponseInfo.builder().status(String.valueOf(HttpStatus.OK.value())).build();
 		return new UserDetailResponse(responseInfo, Collections.singletonList(userRequest));
+	}
+
+	private UpdateResponse createResponseforUpdate(User newUser) {
+		UpdateRequest updateRequest = new UpdateRequest(newUser);
+		ResponseInfo responseInfo = ResponseInfo.builder().status(String.valueOf(HttpStatus.OK.value())).build();
+		return new UpdateResponse(responseInfo, Collections.singletonList(updateRequest));
 	}
 
 	private UserSearchResponse searchUsers(@RequestBody UserSearchRequest request, HttpHeaders headers) {
@@ -174,10 +185,7 @@ public class UserController {
                 searchCriteria.setLimit(defaultSearchSize);
         }
 
-
-		List<User> userModels = userService.searchUsers(searchCriteria, isInterServiceCall(headers));
-
-
+		List<User> userModels = userService.searchUsers(searchCriteria, isInterServiceCall(headers),request.getRequestInfo());
 		List<UserSearchResponseContent> userContracts = userModels.stream().map(UserSearchResponseContent::new)
 				.collect(Collectors.toList());
 		ResponseInfo responseInfo = ResponseInfo.builder().status(String.valueOf(HttpStatus.OK.value())).build();
