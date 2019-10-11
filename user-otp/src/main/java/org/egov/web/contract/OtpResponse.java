a843a19a0ccc123e63090ad5b70803e92f506f01
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-10-11 22:08:17 +0530

    constructor added

--- user-otp/src/main/java/org/egov/web/contract/OtpResponse.java
+++ user-otp/src/main/java/org/egov/web/contract/OtpResponse.java
@@ -3,9 +3,11 @@ package org.egov.web.contract;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OtpResponse {
     private ResponseInfo responseInfo;
     @JsonProperty("isSuccessful")
