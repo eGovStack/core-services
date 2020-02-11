14aaf1c2bf2feaebc290106571af5a26cbaaa7ba
pkranga <rangabashyam.pk@gmail.com>
2020-02-11 15:01:30 +0530

    Update IndexerUtils.java

--- egov-indexer/src/main/java/org/egov/infra/indexer/util/IndexerUtils.java
+++ egov-indexer/src/main/java/org/egov/infra/indexer/util/IndexerUtils.java
@@ -39,7 +39,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
