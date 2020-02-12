5ac95533566a22ab87d2c350191edd099a821df4
gopesh <gopesh.yadav@egovernments.org>
2020-02-12 15:11:00 +0530

    keywords changes

--- chatbot/src/main/java/org/egov/chat/service/ResetCheck.java
+++ chatbot/src/main/java/org/egov/chat/service/ResetCheck.java
@@ -14,6 +14,7 @@ import org.egov.chat.models.egovchatserdes.EgovChatSerdes;
 import org.egov.chat.repository.ConversationStateRepository;
 import org.egov.chat.util.CommonAPIErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -27,7 +28,9 @@ public class ResetCheck {
 
     private String streamName = "reset-check";
 
-    private List<String> resetKeywords = Arrays.asList("mseva", "hi", "hello", "missedCall");
+    @Value("${flow.reset.keywords}")
+    private String resetKeywordsString;
+
     private int fuzzymatchScoreThreshold = 90;
 
     @Autowired
@@ -64,8 +67,7 @@ public class ResetCheck {
     private boolean isResetKeyword(EgovChat chatNode) {
         try {
             String answer = chatNode.getMessage().getRawInput();
-
-            for (String resetKeyword : resetKeywords) {
+            for (String resetKeyword : resetKeywordsString.split(",")) {
                 int score = FuzzySearch.tokenSetRatio(resetKeyword, answer);
                 if (score >= fuzzymatchScoreThreshold)
                     return true;
