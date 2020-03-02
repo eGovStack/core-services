d375ecaaedb65ce4972de6c92c7355c863f189d6
Rushang Dhanesha <rushangdhanesha@gmail.com>
2020-03-02 15:12:25 +0530

    Pre-chatbot combine tenant-enrich and user-data-enrich streams.

--- chatbot/src/main/java/org/egov/chat/pre/controller/PreChatController.java
+++ chatbot/src/main/java/org/egov/chat/pre/controller/PreChatController.java
@@ -1,7 +1,6 @@
 package org.egov.chat.pre.controller;
 
-import org.egov.chat.pre.service.TenantIdEnricher;
-import org.egov.chat.pre.service.UserDataEnricher;
+import org.egov.chat.pre.service.PreChatbotStream;
 import org.egov.chat.util.KafkaTopicCreater;
 import org.egov.chat.xternal.Requestformatter.ValueFirst.ValueFirstRequestFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,29 +10,25 @@ import javax.annotation.PostConstruct;
 
 @Controller
 public class PreChatController {
-//
+
     @Autowired
     private ValueFirstRequestFormatter valueFirstRequestFormatter;
     @Autowired
-    private TenantIdEnricher tenantIdEnricher;
-    @Autowired
-    private UserDataEnricher userDataEnricher;
+    private PreChatbotStream preChatbotStream;
     @Autowired
     private KafkaTopicCreater kafkaTopicCreater;
 
     @PostConstruct
     public void initPreChatbotStreams() {
-//        karixRequestFormatter.startRequestFormatterStream("whatsapp-received-messages",
-//                "transformed-input-messages", "chatbot-error-messages");
+        kafkaTopicCreater.createTopic("whatsapp-received-messages");
         kafkaTopicCreater.createTopic("transformed-input-messages");
         kafkaTopicCreater.createTopic("chatbot-error-messages");
-        kafkaTopicCreater.createTopic("tenant-enriched-messages");
         kafkaTopicCreater.createTopic("input-messages");
-        kafkaTopicCreater.createTopic("whatsapp-received-messages");
+
         valueFirstRequestFormatter.startRequestFormatterStream("whatsapp-received-messages",
                 "transformed-input-messages", "chatbot-error-messages");
-        tenantIdEnricher.startTenantEnricherStream("transformed-input-messages", "tenant-enriched-messages");
-        userDataEnricher.startUserDataStream("tenant-enriched-messages", "input-messages");
+
+        preChatbotStream.startPreChatbotStream("transformed-input-messages", "input-messages");
     }
 
 }
