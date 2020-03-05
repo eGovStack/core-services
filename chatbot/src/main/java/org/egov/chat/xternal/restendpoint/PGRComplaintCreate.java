b0aece9d4ca5af4700a0bf30a35b936f16a9a26f
gopesh <gopesh.yadav@egovernments.org>
2020-03-05 12:02:55 +0530

    send text message instead of image on PGR complaint create

--- chatbot/src/main/java/org/egov/chat/xternal/restendpoint/PGRComplaintCreate.java
+++ chatbot/src/main/java/org/egov/chat/xternal/restendpoint/PGRComplaintCreate.java
@@ -54,27 +54,18 @@ public class PGRComplaintCreate implements RestEndpoint {
     @Override
     public ObjectNode getMessageForRestCall(ObjectNode params) throws Exception {
         String authToken = params.get("authToken").asText();
-        String refreshToken = params.get("refreshToken").asText();
         String mobileNumber = params.get("mobileNumber").asText();
         String complaintType = params.get("pgr.create.complaintType").asText();
         String city = params.get("pgr.create.tenantId").asText();
         String locality = params.get("pgr.create.locality").asText();
         String complaintDetails = params.get("pgr.create.complaintDetails").asText();
         String photo = params.get("pgr.create.photo").asText();
-
         DocumentContext userInfo = JsonPath.parse(params.get("userInfo").asText());
-
-//        String location = params.get("pgr.create.location").asText();
-//        ObjectNode locationNode = (ObjectNode) objectMapper.readTree(location);
-
         WriteContext request = JsonPath.parse(pgrCreateRequestBody);
-
         request.set("$.RequestInfo.authToken", authToken);
         request.set("$.RequestInfo.userInfo", userInfo.json());
         request.set("$.services.[0].city", city);
         request.set("$.services.[0].tenantId", city);
-//        request.set("$.services.[0].addressDetail.latitude", locationNode.get("latitude").asText());
-//        request.set("$.services.[0].addressDetail.longitude", locationNode.get("longitude").asText());
         request.set("$.services.[0].addressDetail.city", city);
         request.set("$.services.[0].addressDetail.mohalla", locality);
         request.set("$.services.[0].serviceCode", complaintType);
@@ -86,26 +77,25 @@ public class PGRComplaintCreate implements RestEndpoint {
             request.add("$.actionInfo.[0].media", photo);
 
         log.info("PGR Create complaint request : " + request.jsonString());
-
         JsonNode requestObject = null;
         requestObject = objectMapper.readTree(request.jsonString());
         ObjectNode responseMessage = objectMapper.createObjectNode();
         responseMessage.put("type", "text");
         ResponseEntity<ObjectNode> response = restTemplate.postForEntity(pgrHost + pgrCreateComplaintPath,
                 requestObject, ObjectNode.class);
-        responseMessage = makeMessageForResponse(response, mobileNumber, photo);
+        responseMessage = makeMessageForResponse(response, mobileNumber);
         responseMessage.put("timestamp", System.currentTimeMillis());
         return responseMessage;
     }
 
-    private ObjectNode makeMessageForResponse(ResponseEntity<ObjectNode> responseEntity, String mobileNumber, String photo) throws Exception {
+    private ObjectNode makeMessageForResponse(ResponseEntity<ObjectNode> responseEntity, String mobileNumber) throws Exception {
         ObjectNode responseMessage = objectMapper.createObjectNode();
-        if (!photo.equalsIgnoreCase("null")) {
-            responseMessage.put("type", "image");
-            responseMessage.put("fileStoreId", photo);
-        } else {
-            responseMessage.put("type", "text");
-        }
+//        if (!photo.equalsIgnoreCase("null")) {
+//            responseMessage.put("type", "image");
+//            responseMessage.put("fileStoreId", photo);
+//        } else {
+        responseMessage.put("type", "text");
+//        }
         if (responseEntity.getStatusCode().is2xxSuccessful()) {
             ObjectNode pgrResponse = responseEntity.getBody();
             String complaintNumber = pgrResponse.get("services").get(0).get("serviceRequestId").asText();
@@ -113,10 +103,7 @@ public class PGRComplaintCreate implements RestEndpoint {
             String url = egovExternalHost + "citizen/otpLogin?mobileNo=" + mobileNumber + "&redirectTo=complaint-details/" + encodedPath + "?";
             String shortenedURL = urlShorteningService.shortenURL(url);
             ObjectNode param;
-
             ObjectNode params = objectMapper.createObjectNode();
-
-
             ArrayNode localizationCodeArray = objectMapper.valueToTree(
                     numeralLocalization.getLocalizationCodesForStringContainingNumbers(complaintNumber));
             params.set("complaintNumber", localizationCodeArray);
@@ -124,7 +111,6 @@ public class PGRComplaintCreate implements RestEndpoint {
             param = objectMapper.createObjectNode();
             param.put("value", shortenedURL);
             params.set("url", param);
-
             ObjectNode template = objectMapper.createObjectNode();
             template.put("templateId", localizationTemplateCode);
             template.set("params", params);
