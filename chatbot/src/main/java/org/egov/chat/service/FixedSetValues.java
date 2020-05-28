91f65bbb45b2c26a3f98972bbd5cf073c4023f90
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2020-05-28 14:58:41 +0530

    Feature: Allow question of FixedSetValues with multiple answers. (Only numeric comma-separated input accepted)

--- chatbot/src/main/java/org/egov/chat/service/FixedSetValues.java
+++ chatbot/src/main/java/org/egov/chat/service/FixedSetValues.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -94,6 +95,7 @@ public class FixedSetValues {
 
     public EgovChat extractAnswer(JsonNode config, EgovChat chatNode) throws IOException {
         boolean displayValuesAsOptions = config.get("displayValuesAsOptions") != null && config.get("displayValuesAsOptions").asBoolean();
+        boolean multipleAnswersAllowed = config.get("multipleAnswers") != null && config.get("multipleAnswers").asBoolean();
 
         String answer = chatNode.getMessage().getRawInput();
         ConversationState conversationState = getConversationStateForChat(chatNode);
@@ -114,7 +116,26 @@ public class FixedSetValues {
 
         Integer answerIndex = null;
         Boolean reQuestion = false;
-        if (displayValuesAsOptions && (answer.equalsIgnoreCase(nextKeyword) || answer.equalsIgnoreCase(nextKeywordSymbol))) {
+        String finalAnswer = null;
+        if(multipleAnswersAllowed && checkIfInputContainsMultipleChoices(answer)) {
+            List<String> answers = new ArrayList<>();
+            List<Integer> indices = getMultipleAnswerIndices(answer);
+            for(Integer index : indices) {
+                String value = "";
+                JsonNode answerLocalizationCode = validValues.get(index);
+                log.debug("answerLocalizationCode  : " + answerLocalizationCode);
+                if (answerLocalizationCode.has("code"))
+                    value = answerLocalizationCode.get("code").asText();
+                else if (answerLocalizationCode.has("value"))
+                    value = answerLocalizationCode.get("value").asText();
+                answers.add(valueFetcher.getCodeForValue(config, chatNode, value));
+            }
+            finalAnswer = "";
+            for(String string : answers) {
+                finalAnswer += string + ", ";
+            }
+            finalAnswer = finalAnswer.substring(0, finalAnswer.length() - 2); //To remove ", "
+        } else if (displayValuesAsOptions && (answer.equalsIgnoreCase(nextKeyword) || answer.equalsIgnoreCase(nextKeywordSymbol))) {
             reQuestion = true;
         } else if (displayValuesAsOptions && checkIfAnswerIsIndex(answer)) {
             answerIndex = Integer.parseInt(answer) - 1;
@@ -135,13 +156,10 @@ public class FixedSetValues {
             }
         }
 
-        log.debug("Answer Index : " + answerIndex);
-
-        String finalAnswer = null;
         if (reQuestion) {
             chatNode.setAskForNextBatch(true);
             finalAnswer = nextKeyword;
-        } else {
+        } else if(!(multipleAnswersAllowed && checkIfInputContainsMultipleChoices(answer))) {
             JsonNode answerLocalizationCode = validValues.get(answerIndex);
             log.debug("answerLocalizationCode  : " + answerLocalizationCode);
             if (answerLocalizationCode.has("code"))
@@ -157,6 +175,19 @@ public class FixedSetValues {
         return chatNode;
     }
 
+    private List<Integer> getMultipleAnswerIndices(String answer) {
+        String[] answers = answer.split(",");
+        List<Integer> indices = new ArrayList<>();
+        for(String string: answers) {
+            indices.add(Integer.parseInt(string));
+        }
+        return indices;
+    }
+
+    boolean checkIfInputContainsMultipleChoices(String answer) {
+        return answer.indexOf(',') != -1;
+    }
+
     private boolean checkIfAnswerIsIndex(String answer) {
         return StringUtils.isNumeric(answer.trim());
     }
@@ -168,17 +199,16 @@ public class FixedSetValues {
     }
 
     public boolean isValid(JsonNode config, EgovChat chatNode) throws IOException {
-        boolean displayValuesAsOptions = config.get("displayValuesAsOptions") != null && config.get("displayValuesAsOptions").asBoolean();
-        String answer = chatNode.getMessage().getRawInput();
+        try {
+            boolean displayValuesAsOptions = config.get("displayValuesAsOptions") != null && config.get("displayValuesAsOptions").asBoolean();
+            boolean multipleAnswersAllowed = config.get("multipleAnswers") != null && config.get("multipleAnswers").asBoolean();
 
-        ConversationState conversationState = getConversationStateForChat(chatNode);
-        JsonNode questionDetails = conversationState.getQuestionDetails();
+            String answer = chatNode.getMessage().getRawInput();
 
-        ArrayNode allValues = (ArrayNode) questionDetails.get("allValues");
+            ConversationState conversationState = getConversationStateForChat(chatNode);
+            JsonNode questionDetails = conversationState.getQuestionDetails();
 
-        if (displayValuesAsOptions && (answer.equalsIgnoreCase(nextKeyword) || answer.equalsIgnoreCase(nextKeywordSymbol))) {
-            return true;
-        } else if (displayValuesAsOptions && checkIfAnswerIsIndex(answer)) {
+            ArrayNode allValues = (ArrayNode) questionDetails.get("allValues");
             Integer offset = questionDetails.get("offset").asInt();
             Integer batchSize = questionDetails.get("batchSize").asInt();
             Integer upperLimit = Math.min(offset + batchSize, allValues.size());
@@ -186,21 +216,33 @@ public class FixedSetValues {
             for (int i = 0; i < upperLimit; i++) {
                 validValues.add(allValues.get(i));
             }
-            return checkIfIndexIsValid(answer, validValues);
-        } else {
-            String locale = chatNode.getConversationState().getLocale();
-            List<LocalizationCode> localizationCodes = Arrays.asList(objectMapper.convertValue(allValues, LocalizationCode[].class));
-            List<String> localizedValidValues = localizationService.getMessagesForCodes(localizationCodes, locale);
-            return fuzzyMatchAnswerWithValidValues(answer, localizedValidValues, config);
-        }
-    }
 
-    boolean checkIfIndexIsValid(String answer, ArrayNode validValues) {
-        Integer answerInteger = Integer.parseInt(answer);
-        if (answerInteger > 0 && answerInteger <= validValues.size())
-            return true;
-        else
+            if (multipleAnswersAllowed && checkIfInputContainsMultipleChoices(answer)) {
+                List<Integer> indices = getMultipleAnswerIndices(answer);
+                boolean isValid = true;
+                for (Integer index : indices) {
+                    if (index < 0 || index >= validValues.size()) {
+                        isValid = false;
+                    }
+                }
+                return isValid;
+            }
+
+            if (displayValuesAsOptions && (answer.equalsIgnoreCase(nextKeyword) || answer.equalsIgnoreCase(nextKeywordSymbol))) {
+                return true;
+            } else if (displayValuesAsOptions && checkIfAnswerIsIndex(answer)) {
+                Integer answerInteger = Integer.parseInt(answer);
+                return answerInteger > 0 && answerInteger <= validValues.size();
+            } else {
+                String locale = chatNode.getConversationState().getLocale();
+                List<LocalizationCode> localizationCodes = Arrays.asList(objectMapper.convertValue(allValues, LocalizationCode[].class));
+                List<String> localizedValidValues = localizationService.getMessagesForCodes(localizationCodes, locale);
+                return fuzzyMatchAnswerWithValidValues(answer, localizedValidValues, config);
+            }
+        } catch (Exception e) {
+            log.error("Error when checking validity : ", e);
             return false;
+        }
     }
 
     boolean fuzzyMatchAnswerWithValidValues(String answer, List<String> validValues, JsonNode config) {
