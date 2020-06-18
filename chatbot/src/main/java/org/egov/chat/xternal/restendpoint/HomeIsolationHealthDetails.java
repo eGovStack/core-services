888b7bfb3d6444cebb41949e661eaa5a6c947fd1
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2020-06-18 17:24:48 +0530

    HealthDetailsCreate handle error response

--- chatbot/src/main/java/org/egov/chat/xternal/restendpoint/HomeIsolationHealthDetails.java
+++ chatbot/src/main/java/org/egov/chat/xternal/restendpoint/HomeIsolationHealthDetails.java
@@ -13,7 +13,9 @@ import org.egov.chat.service.restendpoint.RestEndpoint;
 import org.egov.chat.util.NumeralLocalization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -45,6 +47,7 @@ public class HomeIsolationHealthDetails implements RestEndpoint {
     private String healthDetailsCreateRequest = "{\"RequestInfo\":{\"userInfo\":{}},\"tenantId\":\"\",\"mobileNumber\":\"\",\"healthDetails\":[{\"temperature\":98.6,\"symptoms\":\"\",\"breathingIssues\":false,\"fever\":false,\"dryCough\":false}]}";
 
     private String responseMessageTemplateId = "chatbot.home.isolation.health.detail.create.message";
+    private String responseMessageForAlreadySubmittedHealthDetails = "chatbot.home.isolation.health.detail.create.HEALTH_DETAILS_SUBMITTED";
 
     @Override
     public ObjectNode getMessageForRestCall(ObjectNode params) throws Exception {
@@ -68,22 +71,32 @@ public class HomeIsolationHealthDetails implements RestEndpoint {
         writeContext = addSymptomsAsBoolean(symptoms, writeContext);
 
         JsonNode requestJson = objectMapper.readTree(writeContext.jsonString());
+        ResponseEntity<JsonNode> responseEntity = null;
+        LocalizationCode localizationCode = null;
+        try {
+            responseEntity = restTemplate.postForEntity(
+                    caseManagementServiceHost + healthDetailCreateEndpoint, requestJson, JsonNode.class);
 
-        JsonNode responseJson = restTemplate.postForObject(caseManagementServiceHost + healthDetailCreateEndpoint,
-                requestJson, JsonNode.class);
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            String dateString = formatter.format(date);
 
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        String dateString = formatter.format(date);
+            ObjectNode templateParams = objectMapper.createObjectNode();
 
-        ObjectNode templateParams = objectMapper.createObjectNode();
+            ObjectNode localizationCodeDate = objectMapper.createObjectNode();
+            localizationCodeDate.put("value", dateString);
+            templateParams.set("date", localizationCodeDate);
 
-        ObjectNode localizationCodeDate = objectMapper.createObjectNode();
-        localizationCodeDate.put("value", dateString);
-        templateParams.set("date", localizationCodeDate);
+            localizationCode = LocalizationCode.builder().templateId(responseMessageTemplateId)
+                    .params(templateParams).build();
+        } catch (HttpClientErrorException e) {
+            localizationCode = LocalizationCode.builder().code(responseMessageForAlreadySubmittedHealthDetails).build();
+            JsonNode responseJson = objectMapper.readTree(e.getResponseBodyAsString());
+            String errorCode = responseJson.at("/Errors/0/code").asText();
+            log.info("Error response from case management service : " + responseJson.toString());
+            log.info("Error Code : " + errorCode);
+        }
 
-        LocalizationCode localizationCode = LocalizationCode.builder().templateId(responseMessageTemplateId)
-                .params(templateParams).build();
         Response response = Response.builder().type("text")
                 .localizationCodes(Collections.singletonList(localizationCode)).build();
 
