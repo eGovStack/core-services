52dd5c6292924f19549472b71297d82b19008991
mahammadnizam-wt-eGov <mahammad.nizam@egovernments.org>
2020-06-19 14:56:32 +0530

    Added business detail for configring bank account for module wise for a tenant

--- egov-pg-service/src/main/java/org/egov/pg/models/Transaction.java
+++ egov-pg-service/src/main/java/org/egov/pg/models/Transaction.java
@@ -25,6 +25,11 @@ import java.util.Map;
 @Builder
 @ToString
 public class Transaction {
+	
+	@JsonProperty("businessDetail")
+	@NotNull
+	@Size(min = 1)
+	private String businessDetail;
 
     @JsonProperty("tenantId")
     @NotNull
