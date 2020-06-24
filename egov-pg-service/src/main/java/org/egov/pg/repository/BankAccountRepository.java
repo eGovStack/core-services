7d0e247e1014a22cfc0aa5efbcd764d814625d29
mahammadnizam-wt-eGov <mahammad.nizam@egovernments.org>
2020-06-24 15:24:56 +0530

    Changed businessdetail to businessService

--- egov-pg-service/src/main/java/org/egov/pg/repository/BankAccountRepository.java
+++ egov-pg-service/src/main/java/org/egov/pg/repository/BankAccountRepository.java
@@ -29,10 +29,10 @@ public class BankAccountRepository {
         this.appProperties = appProperties;
     }
 
-    public BankAccount getBankAccountsById( RequestInfo requestInfo, String tenantId, String businessDetail) {
+    public BankAccount getBankAccountsById( RequestInfo requestInfo, String tenantId, String businessService) {
         MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
         queryParams.add("tenantId", tenantId);
-        queryParams.add("businessDetail", businessDetail);
+        queryParams.add("businessService", businessService);
 
         String uri = UriComponentsBuilder
                 .fromHttpUrl(appProperties.getBankAccountHost())
