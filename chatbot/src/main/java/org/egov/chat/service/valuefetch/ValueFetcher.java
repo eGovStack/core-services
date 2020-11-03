87bf2fa96a0c57dc42d23a19e8d18ef27bcbbcf3
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2020-11-03 19:04:04 +0530

    Improved Error Handling

--- chatbot/src/main/java/org/egov/chat/service/valuefetch/ValueFetcher.java
+++ chatbot/src/main/java/org/egov/chat/service/valuefetch/ValueFetcher.java
@@ -121,11 +121,6 @@ public class ValueFetcher {
                 return message.getMessageContent();
             }
         }
-        //If nodeId isn't found in previously saved messages in DB
-        //Try to find in the last received message
-        if (chatNode.at("/message/nodeId").asText().equalsIgnoreCase(nodeId)) {
-            return chatNode.at("/message/messageContent").asText();
-        }
         return null;
     }
 
