90dbda6d44438535874f844d58f3040094a64f57
Tarun Lalwani <tarunlalwani@gmail.com>
2020-12-01 02:40:30 +0530

    Add one more cache for location

--- egov-location/src/main/java/org/egov/boundary/persistence/repository/MdmsRepository.java
+++ egov-location/src/main/java/org/egov/boundary/persistence/repository/MdmsRepository.java
@@ -46,6 +46,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -80,6 +81,7 @@ public class MdmsRepository {
 	    this.masterName = masterName;			
 	}
 
+	@Cacheable(value = "mdmsCache", sync = true, key = "{#tenantId, #hierarchyTypeCode}")
 	public JSONArray getByCriteria(String tenantId,String hierarchyTypeCode,RequestInfo requestInfo) {
 		
 		MasterDetails[] masterDetails;
