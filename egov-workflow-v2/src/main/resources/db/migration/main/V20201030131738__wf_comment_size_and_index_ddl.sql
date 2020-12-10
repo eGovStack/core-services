cff12f2d63909ed286316e796581c85f62b91ed8
talele08 <talele.aniket@gmail.com>
2020-12-10 18:09:19 +0530

    added index and increased comment size

---
+++ egov-workflow-v2/src/main/resources/db/migration/main/V20201030131738__wf_comment_size_and_index_ddl.sql
@@ -0,0 +1,4 @@
+CREATE INDEX IF NOT EXISTS  idx_eg_wf_assignee_v2_assignee on eg_wf_assignee_v2(tenantid, assignee)
+CREATE INDEX IF NOT EXISTS  idx_tenant_status_eg_wf_processinstance_v2 ON eg_wf_processinstance_v2 USING btree ((tenantid || ':' || status));
+
+ALTER TABLE eg_wf_processinstance_v2 ALTER COLUMN comment type character varying(1024);
