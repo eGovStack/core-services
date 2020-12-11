b23044d3dc171bfdb6d26a2dec04c73e539ba87e
Tarun Lalwani <tarunlalwani@gmail.com>
2020-12-11 12:57:06 +0530

    Make upload of receipt async

--- pdf-service/src/index.js
+++ pdf-service/src/index.js
@@ -4,7 +4,9 @@ import request from "request";
 import express from "express";
 import logger from "./config/logger";
 import path from "path";
-import fs, { exists } from "fs";
+import fs, {
+  exists
+} from "fs";
 import axios from "axios";
 import cors from "cors";
 import morgan from "morgan";
@@ -14,17 +16,36 @@ import * as pdfmake from "pdfmake/build/pdfmake";
 import * as pdfFonts from "pdfmake/build/vfs_fonts";
 import get from "lodash/get";
 import set from "lodash/set";
-import { strict } from "assert";
-import { Recoverable } from "repl";
-import { fileStoreAPICall } from "./utils/fileStoreAPICall";
-import { directMapping } from "./utils/directMapping";
-import { externalAPIMapping } from "./utils/externalAPIMapping";
+import {
+  strict
+} from "assert";
+import {
+  Recoverable
+} from "repl";
+import {
+  fileStoreAPICall
+} from "./utils/fileStoreAPICall";
+import {
+  directMapping
+} from "./utils/directMapping";
+import {
+  externalAPIMapping
+} from "./utils/externalAPIMapping";
 import envVariables from "./EnvironmentVariables";
 import QRCode from "qrcode";
-import { getValue } from "./utils/commons";
-import { getFileStoreIds, insertStoreIds } from "./queries";
-import { listenConsumer } from "./kafka/consumer";
-import { convertFooterStringtoFunctionIfExist } from "./utils/commons";
+import {
+  getValue
+} from "./utils/commons";
+import {
+  getFileStoreIds,
+  insertStoreIds
+} from "./queries";
+import {
+  listenConsumer
+} from "./kafka/consumer";
+import {
+  convertFooterStringtoFunctionIfExist
+} from "./utils/commons";
 
 var jp = require("jsonpath");
 //create binary
@@ -33,8 +54,14 @@ var pdfMakePrinter = require("pdfmake/src/printer");
 
 let app = express();
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.json({ limit: "10mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(bodyParser.json({
+  limit: "10mb",
+  extended: true
+}));
+app.use(bodyParser.urlencoded({
+  limit: "10mb",
+  extended: true
+}));
 
 let maxPagesAllowed = envVariables.MAX_NUMBER_PAGES;
 let serverport = envVariables.SERVER_PORT;
@@ -112,50 +139,54 @@ const createPdfBinary = async (
     var jobid = `${key}${new Date().getTime()}`;
     if (noOfDefinitions == 0) {
       logger.error("no file generated for pdf");
-      errorCallback({ message: " error: no file generated for pdf" });
+      errorCallback({
+        message: " error: no file generated for pdf"
+      });
     } else {
       var dbInsertSingleRecords = [];
       var dbInsertBulkRecords = [];
-      await Promise.all([
+      // instead of awaiting the promise, use process.nextTick to asynchronously upload the receipt
+      //
+      process.nextTick(function () {
         uploadFiles(
-          dbInsertSingleRecords,
-          dbInsertBulkRecords,
-          formatconfig,
-          listDocDefinition,
-          key,
-          false,
-          jobid,
-          noOfDefinitions,
-          entityIds,
-          starttime,
-          successCallback,
-          errorCallback,
-          tenantId,
-          totalobjectcount,
-          userid,
-          documentType,
-          moduleName
-        ),
-        uploadFiles(
-          dbInsertSingleRecords,
-          dbInsertBulkRecords,
-          formatconfig,
-          listDocDefinition,
-          key,
-          true,
-          jobid,
-          noOfDefinitions,
-          entityIds,
-          starttime,
-          successCallback,
-          errorCallback,
-          tenantId,
-          totalobjectcount,
-          userid,
-          documentType,
-          moduleName
-        ),
-      ]);
+            dbInsertSingleRecords,
+            dbInsertBulkRecords,
+            formatconfig,
+            listDocDefinition,
+            key,
+            false,
+            jobid,
+            noOfDefinitions,
+            entityIds,
+            starttime,
+            successCallback,
+            errorCallback,
+            tenantId,
+            totalobjectcount,
+            userid,
+            documentType,
+            moduleName
+          ),
+          uploadFiles(
+            dbInsertSingleRecords,
+            dbInsertBulkRecords,
+            formatconfig,
+            listDocDefinition,
+            key,
+            true,
+            jobid,
+            noOfDefinitions,
+            entityIds,
+            starttime,
+            successCallback,
+            errorCallback,
+            tenantId,
+            totalobjectcount,
+            userid,
+            documentType,
+            moduleName
+          )
+      });
     }
   } catch (err) {
     logger.error(err.stack || err);
@@ -293,10 +324,9 @@ const uploadFiles = async (
         .catch((err) => {
           logger.error(err.stack || err);
           errorCallback({
-            message:
-              "error occurred while uploading pdf: " + (typeof err === "string")
-                ? err
-                : err.message,
+            message: "error occurred while uploading pdf: " + (typeof err === "string") ?
+              err :
+              err.message,
           });
         });
     });
@@ -452,7 +482,10 @@ app.post(
             // doc successfully created
             res.status(responseBody.status);
             delete responseBody.status;
-            res.json({ ResponseInfo: requestInfo, ...responseBody });
+            res.json({
+              ResponseInfo: requestInfo,
+              ...responseBody
+            });
           }
         );
       }
@@ -619,10 +652,9 @@ export const createAndSave = async (
     ).catch((err) => {
       logger.error(err.stack || err);
       errorCallback({
-        message:
-          "error occurred in createPdfBinary call: " + (typeof err === "string")
-            ? err
-            : err.message,
+        message: "error occurred in createPdfBinary call: " + (typeof err === "string") ?
+          err :
+          err.message,
       });
     });
   }
@@ -753,7 +785,9 @@ const prepareBegin = async (
   var entityIdPath = get(dataconfig, "DataConfigs.entityIdPath");
   if (baseKeyPath == null) {
     logger.error("baseKeyPath is absent in config");
-    throw { message: `baseKeyPath is absent in config` };
+    throw {
+      message: `baseKeyPath is absent in config`
+    };
   }
   return await prepareBulk(
     key,
