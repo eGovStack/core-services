682986ec2c2601204cd7af00590d0c4df083e4a2
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2021-01-17 13:25:00 +0530

    Use PostgreSQL as the detault repo provider

--- xstate-chatbot/nodejs/src/session/repo/index.js
+++ xstate-chatbot/nodejs/src/session/repo/index.js
@@ -3,11 +3,11 @@ const postgresRepo = require('./postgres-repo');
 const inMemoryRepo = require('./in-memory-repo');
 
 console.log(`Found repoProvider <${config.repoProvider}>`);
-if(config.repoProvider === 'PostgreSQL') {
-    console.log("Using PostgreSQL Repo");
-    module.exports = postgresRepo;
-}
-else {
+if(config.repoProvider === 'InMemory') {
     console.log("Using In-memory Repo (default)");
     module.exports = inMemoryRepo;
+}
+else {
+    console.log("Using PostgreSQL Repo");
+    module.exports = postgresRepo;
 }
