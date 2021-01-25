49e007e0437d460a77b2ae0e900b2201dd887ca6
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2021-01-25 11:29:40 +0530

    Add id param in telemetry. Alert: New library installed

--- xstate-chatbot/nodejs/src/session/telemetry.js
+++ xstate-chatbot/nodejs/src/session/telemetry.js
@@ -1,9 +1,11 @@
 const config = require('../env-variables');
 const producer = require('./kafka/kafka-producer');
+const uuid = require('uuid');
 
 class Telemetry {
     async log(userId, type, data) {
         let object = {
+            id: uuid.v4(),
             date: new Date().getTime(),
             user: userId,
             type: type,
