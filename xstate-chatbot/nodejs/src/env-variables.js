777f12a1d365959f2ddea24d33f1605cbde399dc
Rushang Dhanesha <rushang.dhanesha@egovernments.org>
2021-01-28 14:15:34 +0530

    Handle multiple pgr versions

--- xstate-chatbot/nodejs/src/env-variables.js
+++ xstate-chatbot/nodejs/src/env-variables.js
@@ -87,6 +87,7 @@ const envVariables = {
     },
 
     pgrUseCase: {
+        pgrVersion: process.env.PGR_VERSION || 'v2',
         complaintSearchLimit: process.env.COMPLAINT_SEARCH_LIMIT || 5,
         informationImageFilestoreId: process.env.INFORMATION_IMAGE_FILESTORE_ID || '1844872a-9cb7-4464-bcff-a1c8c9bb8799',
         pgrUpdateTopic: process.env.PGR_UPDATE_TOPIC || 'update-pgr-request',
