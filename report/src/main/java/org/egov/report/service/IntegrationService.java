976508e2f6fcda2825da29d337a8c7ea2f5e05a1
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2021-04-23 11:15:16 +0530

    RAIN-2538 made changes in core service repo to take value of timezone from application.properties file (#655)

--- report/src/main/java/org/egov/report/service/IntegrationService.java
+++ report/src/main/java/org/egov/report/service/IntegrationService.java
@@ -16,6 +16,7 @@ import org.egov.swagger.model.MetadataResponse;
 import org.egov.swagger.model.ReportDefinition;
 import org.egov.swagger.model.SearchColumn;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,6 +29,9 @@ public class IntegrationService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Value("${id.timezone}")
+    private String timezone;
 
     public MetadataResponse getData(ReportDefinition reportDefinition, MetadataResponse metadataResponse, RequestInfo requestInfo, String moduleName) {
 
@@ -141,7 +145,7 @@ public class IntegrationService {
 
     public long getCurrentTime() {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
+        calendar.setTimeZone(TimeZone.getTimeZone(timezone));
         return calendar.getTimeInMillis();
     }
 }
