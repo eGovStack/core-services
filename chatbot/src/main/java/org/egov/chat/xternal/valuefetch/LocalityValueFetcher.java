ab845ca1cc314e9951682b5c7350cc2d01d4c4d5
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2021-04-23 12:02:24 +0530

    RAIN-2538 Added missing library (#657)

--- chatbot/src/main/java/org/egov/chat/xternal/valuefetch/LocalityValueFetcher.java
+++ chatbot/src/main/java/org/egov/chat/xternal/valuefetch/LocalityValueFetcher.java
@@ -9,6 +9,8 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.egov.chat.service.valuefetch.ExternalValueFetcher;
 import org.egov.chat.util.URLShorteningSevice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
