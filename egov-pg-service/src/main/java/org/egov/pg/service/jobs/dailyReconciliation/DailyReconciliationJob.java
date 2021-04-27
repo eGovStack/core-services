a22cae4f5f26a7809c0784c5f3e8d9a3942d6a4b
mohaarif007 <48949208+mohaarif007@users.noreply.github.com>
2021-04-27 16:24:35 +0530

    update daily Recon with time limit
    update daily Recon with time limit

--- egov-pg-service/src/main/java/org/egov/pg/service/jobs/dailyReconciliation/DailyReconciliationJob.java
+++ egov-pg-service/src/main/java/org/egov/pg/service/jobs/dailyReconciliation/DailyReconciliationJob.java
@@ -51,8 +51,9 @@ public class DailyReconciliationJob implements Job {
      */
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
-        List<Transaction> pendingTxns = transactionRepository.fetchTransactions(TransactionCriteria.builder()
-                 .txnStatus(Transaction.TxnStatusEnum.PENDING).build());
+        List<Transaction> pendingTxns = transactionRepository.fetchTransactionsByTimeRange(TransactionCriteria.
+		  builder() .txnStatus(Transaction.TxnStatusEnum.PENDING).build(), 0L,
+		  System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(appProperties.getEarlyReconcileJobRunInterval () * 2));
 
         log.info("Attempting to reconcile {} pending transactions", pendingTxns.size());
 
