3bd486b6ac7c39a7b64af1f9e95efca29c1b0518
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2021-05-14 18:26:59 +0530

    vitals and homeisolation apis

--- xstate-chatbot/nodejs/src/machine/service/cova-person-service.js
+++ xstate-chatbot/nodejs/src/machine/service/cova-person-service.js
@@ -1,13 +1,41 @@
+const fetch = require("node-fetch");
+const config = require("../../env-variables");
+
 class PersonService {
 
+  /**
+   * Method to check if a patient is in hoemIsolation based on their given mobilenumber
+   * 
+   * @param {*} mobileNumber 
+   */
   async isHomeIsolatedPatient(mobileNumber) {
+
+    let url = config.covaApiConfigs.covaProdUrl.concat(
+      config.covaApiConfigs.isHomeIsolatedSuffix
+    );
+
+    let headers = {
+      "Content-Type": "application/json",
+      Authorization: config.covaApiConfigs.covaAuthorization,
+    };
+
+    var urlSearchParams = new URLSearchParams();
+    urlSearchParams.append("MobileNumber", mobileNumber);
+    urlSearchParams.append("Token", config.covaApiConfigs.covaAuthToken);
+
+    var request = {
+      method: "POST",
+      headers: headers,
+      body: urlSearchParams,
+    };
+
+    await fetch(url, request);
 
   }
 
   async fetchAllHomeIsolatedPatients() {
 
-  }
-
+      }
 }
 
 module.exports = new PersonService();
--- xstate-chatbot/nodejs/src/machine/service/cova-vitals-service.js
+++ xstate-chatbot/nodejs/src/machine/service/cova-vitals-service.js
@@ -1,9 +1,39 @@
 class VitalsService {
 
-  async addVitals(vitals) {
-    
-  }
+  async addVitals(user, vitals) {
 
+    let symptoms = vitals.symptoms;
+    let url = config.covaApiConfigs.covaProdUrl.concat(
+      config.covaApiConfigs.isHomeIsolatedSuffix
+    );
+
+    let headers = {
+      "Content-Type": "application/json",
+      Authorization: config.covaApiConfigs.covaAuthorization,
+    };
+
+    var urlSearchParams = new URLSearchParams();
+
+    urlSearchParams.append("Token",             config.covaApiConfigs.covaAuthToken);
+    urlSearchParams.append("mobile_no",         user.mobileNumber);
+    urlSearchParams.append("pulserate",         vitals.pulse);
+    urlSearchParams.append("spo2level",         vitals.spo2);
+    urlSearchParams.append("current_temp",      vitals.temprature);
+    urlSearchParams.append("Comorbidities",     vitals.comorbidities);
+    urlSearchParams.append("FluLikeSymptoms",   symptoms.has("flu"));
+    urlSearchParams.append("LossOfSmellTaste",  symptoms.has("lostSmellTaste"));
+    urlSearchParams.append("RespiratoryIssues", symptoms.has("respiratoryIssues"));
+    urlSearchParams.append("NeedsDoctorCall",   vitals.NeedsDoctorCall);
+    urlSearchParams.append("Remarks",           vitals.remarks);
+
+    var request = {
+      method: "POST",
+      headers: headers,
+      body: urlSearchParams,
+    };
+
+    await fetch(url, request);
+  }
 }
 
 module.exports = new VitalsService();
