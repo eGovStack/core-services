32615b74a07c3c096c6cf44c0f8e4d440902b9a7
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2021-05-16 13:04:38 +0530

    response handler added for homeisolation

--- xstate-chatbot/nodejs/src/machine/service/cova-person-service.js
+++ xstate-chatbot/nodejs/src/machine/service/cova-person-service.js
@@ -2,14 +2,12 @@ const fetch = require("node-fetch");
 const config = require("../../env-variables");
 
 class PersonService {
-
   /**
    * Method to check if a patient is in hoemIsolation based on their given mobilenumber
-   * 
-   * @param {*} mobileNumber 
+   *
+   * @param {*} mobileNumber
    */
   async isHomeIsolatedPatient(mobileNumber) {
-
     let url = config.covaApiConfigs.covaProdUrl.concat(
       config.covaApiConfigs.isHomeIsolatedSuffix
     );
@@ -29,13 +27,22 @@ class PersonService {
       body: urlSearchParams,
     };
 
-    await fetch(url, request);
+    let response;
 
+    return await fetch(url, request)
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log('The resposne from homeIsolation response : ' + json);
+        let response = JSON.parse(json);
+        if(response == 1)
+        return true;
+        else return false;
+      }).catch(err => console.log('The error msg for homeIsolation : ' + err));
   }
 
-  async fetchAllHomeIsolatedPatients() {
-
-      }
+  async fetchAllHomeIsolatedPatients() {}
 }
 
 module.exports = new PersonService();
