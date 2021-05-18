2a9d0b8ec6ee33f2c6398e0dd605fa42e79f3f7b
Rushang Dhanesha <rushangdhanesha@gmail.com>
2021-05-18 11:52:35 +0530

    Function definitions

--- xstate-chatbot/nodejs/src/machine/service/cova-vitals-service.js
+++ xstate-chatbot/nodejs/src/machine/service/cova-vitals-service.js
@@ -51,6 +51,29 @@ class VitalsService {
       console.error(`Error while registering vitals to Cova.\nStatus: ${response.status}; Response: ${JSON.stringify(responseBody)}`);
     }
   }
+
+  async getCitiesAndMessageBundle() {
+    // TODO Replace dummy data with actual data if district list is available in MDMS
+    let keys = [ 'pb.jalandhar', 'pb.amritsar' ];
+    let messageBundle = {
+      'pb.jalandhar': {
+        en_IN: 'Jalandhar',
+      },
+      'pb.amritsar': {
+        en_IN: 'Amritsar',
+      },
+    };
+    return { keys, messageBundle };
+  }
+
+  async addPatient(user, patientDetails) {
+    // Dummy Content of patient Details
+    //{"name":"asd","age":12,"gender":"male","city":"pb.amritsar","address":"asds","symptomsDate":"2021-05-16T18:30:00.000Z","covidPositiveDate":"2021-05-17T18:30:00.000Z"}
+    // Dates are object Date and not strings 
+
+    // TODO Make API call to Cova
+  }
+
 }
 
 module.exports = new VitalsService();
