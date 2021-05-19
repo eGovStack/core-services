ae95920edca01c3cd1fe7a9762b107dc14473ee6
Rushang Dhanesha <rushangdhanesha@gmail.com>
2021-05-19 18:40:54 +0530

    handle common file for all locales

--- xstate-chatbot/nodejs/src/machine/util/media.js
+++ xstate-chatbot/nodejs/src/machine/util/media.js
@@ -4,18 +4,22 @@ const path = require('path');
 const defaultLocale = 'en_IN';
 
 function createMediaMessage(mediaPath, mediaType, locale, caption = '') {
-    let filePath = `${mediaPath}_${locale}.${mediaType}`;
-    if(!fs.existsSync(path.resolve(__dirname, `../../../${filePath}`))) {
-      console.error(`${mediaPath} for ${locale} not found`);
-      filePath = `${mediaPath}_${defaultLocale}.${mediaType}`;
-    }
-    const mediaMessage =  {
-      type: 'media',
-      output: filePath,
-      mediaType: mediaType,
-      caption: caption
-    }
-    return mediaMessage;
+  let filePath;
+  if(locale)
+    filePath = `${mediaPath}_${locale}.${mediaType}`;
+  else
+    filePath = `${mediaPath}.${mediaType}`;
+  if(!fs.existsSync(path.resolve(__dirname, `../../../${filePath}`))) {
+    console.error(`${mediaPath} for ${locale} not found`);
+    filePath = `${mediaPath}_${defaultLocale}.${mediaType}`;
+  }
+  const mediaMessage =  {
+    type: 'media',
+    output: filePath,
+    mediaType: mediaType,
+    caption: caption
+  }
+  return mediaMessage;
 }
 
 module.exports = { createMediaMessage };
