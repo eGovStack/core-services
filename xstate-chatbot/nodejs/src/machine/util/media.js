7fc622a095793a7b899f8c2ca8bfe1ab61281f47
Rushang Dhanesha <rushangdhanesha@gmail.com>
2021-05-28 16:34:48 +0530

    Exception handling for media file exists check

--- xstate-chatbot/nodejs/src/machine/util/media.js
+++ xstate-chatbot/nodejs/src/machine/util/media.js
@@ -5,14 +5,24 @@ const defaultLocale = 'en_IN';
 
 function createMediaMessage(mediaPath, mediaType, locale, caption = '') {
   let filePath;
-  if(locale)
+  if(locale) {
     filePath = `${mediaPath}_${locale}.${mediaType}`;
-  else
-    filePath = `${mediaPath}.${mediaType}`;
-  if(!fs.existsSync(path.resolve(__dirname, `../../../${filePath}`))) {
-    console.error(`${mediaPath} for ${locale} not found`);
-    filePath = `${mediaPath}_${defaultLocale}.${mediaType}`;
+    let fileExists = false;
+    try {
+      fileExists = fs.existsSync(path.resolve(__dirname, `../../../${filePath}`));
+    } catch(err) {
+      console.error(err);
+    }
+    console.log(fileExists);
+    if(!fileExists) {
+      console.error(`${mediaPath} for ${locale} not found`);
+      filePath = `${mediaPath}_${defaultLocale}.${mediaType}`;
+    }  
   }
+  else {
+    filePath = `${mediaPath}.${mediaType}`;
+  }
+  
   const mediaMessage =  {
     type: 'media',
     output: filePath,
