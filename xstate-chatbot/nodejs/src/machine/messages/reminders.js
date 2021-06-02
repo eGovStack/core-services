6f1c190b8f471ab97463c17ce4936f5354f36d39
rushang7-eGov <rushang.dhanesha@egovernments.org>
2021-06-02 15:54:04 +0530

    reminder message changed (#734) (#735)
    Co-authored-by: egovmis1 <42794309+egovmis1@users.noreply.github.com>

--- xstate-chatbot/nodejs/src/machine/messages/reminders.js
+++ xstate-chatbot/nodejs/src/machine/messages/reminders.js
@@ -1,8 +1,8 @@
 let messages = {
   reminder: {
-    en_IN: 'Hope you are feeling well! Please register your vitals. Our team from health department is monitoring your vitals and in case of need they will contact you. We wish you an early recovery.',
-    pa_IN: 'ਉਮੀਦ ਹੈ ਤੁਸੀਂ ਠੀਕ ਮਹਿਸੂਸ ਕਰ ਰਹੇ ਹੋ! ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੇ ਲੱਛਣਾਂ ਨੂੰ ਰਜਿਸਟਰ ਕਰੋ. ਸਿਹਤ ਵਿਭਾਗ ਦੀ ਟੀਮ ਤੁਹਾਡੇ ਲੱਛਣਾਂ ਦੀ ਨਿਗਰਾਨੀ ਕਰ ਰਹੀ ਹੈ ਅਤੇ ਲੋੜ ਪੈਣ \'ਤੇ ਉਹ ਤੁਹਾਡੇ ਨਾਲ ਸੰਪਰਕ ਕਰਨਗੇ.  ਅਸੀਂ ਤੁਹਾਡੇ ਜਲਦੀ ਠੀਕ ਹੋਣ ਦੀ ਕਾਮਨਾ ਕਰਦੇ ਹਾਂ',
-    hi_IN: 'आशा है आप अच्छा महसूस कर रहे होंगे! कृपया अपने लक्षण दर्ज करें। स्वास्थ्य विभाग की टीम आपके लक्षणों की निगरानी कर रही है और जरूरत पड़ने पर आपसे संपर्क करेगी। हम आपके शीघ्र स्वस्थ होने की कामना करते हैं'
+    en_IN: 'Hope you are feeling well! Please register your vitals. Our team from health department is monitoring your vitals and in case of need they will contact you. You can call 104 in case of emergent need. We wish you an early recovery.',
+    pa_IN: 'ਉਮੀਦ ਹੈ ਤੁਸੀਂ ਠੀਕ ਮਹਿਸੂਸ ਕਰ ਰਹੇ ਹੋ! ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੇ ਲੱਛਣਾਂ ਨੂੰ ਰਜਿਸਟਰ ਕਰੋ. ਸਿਹਤ ਵਿਭਾਗ ਦੀ ਟੀਮ ਤੁਹਾਡੇ ਲੱਛਣਾਂ ਦੀ ਨਿਗਰਾਨੀ ਕਰ ਰਹੀ ਹੈ ਅਤੇ ਲੋੜ ਪੈਣ ਤੇ ਉਹ ਤੁਹਾਡੇ ਨਾਲ ਸੰਪਰਕ ਕਰਨਗੇ. ਐਮਰਜੈਂਸੀ ਲੋੜ ਦੇ ਮਾਮਲੇ ਵਿਚ ਤੁਸੀਂ 104 ਤੇ ਕਾਲ ਕਰ ਸਕਦੇ ਹੋ. ਅਸੀਂ ਤੁਹਾਡੇ ਜਲਦੀ ਠੀਕ ਹੋਣ ਦੀ ਕਾਮਨਾ ਕਰਦੇ ਹਾਂ ',
+    hi_IN: 'आशा है आप अच्छा महसूस कर रहे होंगे! कृपया अपने लक्षण दर्ज करें। स्वास्थ्य विभाग की टीम आपके लक्षणों की निगरानी कर रही है और जरूरत पड़ने पर आपसे संपर्क करेगी। आपात स्थिति में आप 104 . पर कॉल कर सकते हैं।  हम आपके शीघ्र स्वस्थ होने की कामना करते हैं'
      }
 };
 
