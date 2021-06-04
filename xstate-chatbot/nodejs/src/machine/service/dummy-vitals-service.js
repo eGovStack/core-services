4f74e24f6be527d3c9d37e11f3fed5a91ef53b25
rushang7-eGov <rushang.dhanesha@egovernments.org>
2021-06-04 13:19:30 +0530

    Revert "Covid chatbot pmidc team (#753)" (#754)
    This reverts commit e4033057bfadc1460a8bcdaaf3957b6af4b0fea4.

--- xstate-chatbot/nodejs/src/machine/service/dummy-vitals-service.js
+++ xstate-chatbot/nodejs/src/machine/service/dummy-vitals-service.js
@@ -14,9 +14,6 @@ class VitalsService {
     return {"response":1,"sys_message":"success","data":[{"Response":"1","mobile_no":"9123123123","patient_name":"","flag":"WHATSPP-PM","patient_Id":"123"}]};
   }
 
-  async getPatientDetailsFromMobileNumber(mobileNumber) {return { "response": 1,"sys_message": "success","data": [{ "MASTER_ID": "2066248","MASTER_NAME": "RENU BALA","com_status": "0","fateh_kit_delivered": "NO"},{"MASTER_ID": "2102986","MASTER_NAME": "GURDIAL SINGH SO BALVIR SINGH","com_status": "1","fateh_kit_delivered": "YES"}] };
-  }
-
-   }
+}
 
 module.exports = new VitalsService();
