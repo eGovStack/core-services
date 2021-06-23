6e3897679dfa3e3cea8180df8ee23b2030d167bd
talele08 <talele08@users.noreply.github.com>
2021-06-23 10:42:58 +0530

    Rain 2279 (#816)
    * RAIN-2279: resolving merge conflicts
    
    * RAIN-2279: updated POM and changelog

--- egov-workflow-v2/src/main/java/org/egov/wf/service/BusinessMasterService.java
+++ egov-workflow-v2/src/main/java/org/egov/wf/service/BusinessMasterService.java
@@ -1,5 +1,6 @@
 package org.egov.wf.service;
 
+import com.jayway.jsonpath.JsonPath;
 import org.egov.wf.config.WorkflowConfig;
 import org.egov.wf.producer.Producer;
 import org.egov.wf.repository.BusinessServiceRepository;
@@ -9,9 +10,15 @@ import org.egov.wf.web.models.BusinessServiceSearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.egov.wf.util.WorkflowConstants.JSONPATH_BUSINESSSERVICE_STATELEVEL;
 
 @Service
 public class BusinessMasterService {
@@ -24,14 +31,17 @@ public class BusinessMasterService {
 
     private BusinessServiceRepository repository;
 
+    private MDMSService mdmsService;
+
 
     @Autowired
     public BusinessMasterService(Producer producer, WorkflowConfig config, EnrichmentService enrichmentService,
-                                 BusinessServiceRepository repository) {
+                                 BusinessServiceRepository repository, MDMSService mdmsService) {
         this.producer = producer;
         this.config = config;
         this.enrichmentService = enrichmentService;
         this.repository = repository;
+        this.mdmsService = mdmsService;
     }
 
 
@@ -42,7 +52,10 @@ public class BusinessMasterService {
      * @param request The BusinessServiceRequest to be persisted
      * @return The enriched object which is persisted
      */
-    @CacheEvict("businessService")
+    @Caching(evict = {
+            @CacheEvict("businessService"),
+            @CacheEvict("roleTenantAndStatusesMapping")
+    })
     public List<BusinessService> create(BusinessServiceRequest request){
        enrichmentService.enrichCreateBusinessService(request);
        producer.push(config.getSaveBusinessServiceTopic(),request);
@@ -56,15 +69,19 @@ public class BusinessMasterService {
      */
     @Cacheable(value = "businessService")
     public List<BusinessService> search(BusinessServiceSearchCriteria criteria){
-        String tenantId = criteria.getTenantIds().get(0);
+        String tenantId = criteria.getTenantId();
         List<BusinessService> businessServices = repository.getBusinessServices(criteria);
-        if(config.getIsStateLevel()){
-            enrichmentService.enrichTenantIdForStateLevel(tenantId,businessServices);
-        }
+        enrichmentService.enrichTenantIdForStateLevel(tenantId,businessServices);
+
         return businessServices;
     }
 
-    @CacheEvict("businessService")
+
+    
+    @Caching(evict = {
+            @CacheEvict("businessService"),
+            @CacheEvict("roleTenantAndStatusesMapping")
+    })
     public List<BusinessService> update(BusinessServiceRequest request){
         enrichmentService.enrichUpdateBusinessService(request);
         producer.push(config.getUpdateBusinessServiceTopic(),request);
