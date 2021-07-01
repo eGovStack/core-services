eafaf856b6c5486b07d51aa6962ab72f311722ca
Sarvesh-eGov <71336235+Sarvesh-eGov@users.noreply.github.com>
2021-07-02 01:08:19 +0530

    Code cleanup
    Naming changed as per conventions

--- nlp-engine/src/auto_trial.py
+++ nlp-engine/src/auto_trial.py
@@ -7,134 +7,109 @@ from fuzzywuzzy import fuzz
 import requests
 import json
 
-#stopwords=open('stopwords.txt').readlines()
 r_stop=requests.get('https://raw.githubusercontent.com/egovernments/egov-mdms-data/DEV/data/pb/Chatbot/stopwords.txt')
 stopwords=r_stop.text.splitlines()
 
-#for i in range(0,len(stopwords)):
-#    stopwords[i]= stopwords[i][0:len(stopwords[i])-1].lower()
-
 rem=stopwords
-#rem= rem+['show','see']
 keep=['are','to','be','not']
 from autocorrect import Speller
 spell=Speller(lang='en')
 
-r= requests.get('https://raw.githubusercontent.com/egovernments/egov-mdms-data/DEV/data/pb/Chatbot/KeyWords.json')
-res=json.loads(r.text)
+keyWords= requests.get('https://raw.githubusercontent.com/egovernments/egov-mdms-data/DEV/data/pb/Chatbot/KeyWords.json')
+result=json.loads(keyWords.text)
+
+toBePaid=['to be paid', 'to be paid','not paid yet','not yet paid']
+
+synonyms=result['KeyWords'][0]['synonyms']
+
+antonyms=result['KeyWords'][0]['antonyms']
+
+quitSynonyms=result['KeyWords'][0]['quits']
+
+pastSynonyms=res['KeyWords'][0]['syn_past']
+
+notPaid=res['KeyWords'][0]['bigrams'][0]['values']
+
+for i in range(len(notPaid)):
+    notPaid[i]=result['KeyWords'][0]['bigrams'][0]['prefix']+' '+notPaid[i]
+
+notDue=res['KeyWords'][0]['bigrams'][1]['values']
+
+for i in range(len(notDue)):
+    notDue[i]=result['KeyWords'][0]['bigrams'][1]['prefix']+' '+notDue[i]
+
+notToBePaid=result['KeyWords'][0]['bigrams'][2]['values']
+
+for i in range(len(notToBePaid)):
+    notToBePaid[i]=result['KeyWords'][0]['bigrams'][2]['prefix']+' '+notToBePaid[i]
 
 
-#syn=['pay','give','pay_up','ante_up','yield','bear','pay_off','make_up','compensate','devote','paid','nonrecreational','gainful','paying','given']
-#ant= ['outstanding','remaining','due','balance','pending','incomplete','exhausted','default','unpaid']
-#quits= ['quit', 'quits', 'leave', 'left', 'back', 'exit','exits','return','returns','discontinue','cease','drop','stop','stopped']
-#syn_past=['past','previous','old','history','complete','completed','done','preceding','former','late','chronicle','ended','completed','accomplished','realized','receipt','receipts']
-#not_paid=['not paid', 'not complete', 'not done', 'not given','not past','not old','not completed','not ended','not accomplished','not realized']
-#not_due=['not due', 'not remaining', 'not pending', 'not outstanding', 'not balance','not incomplete','not default','not unpaid']
-#not_to_be_paid= ['not to be paid', 'not to be given']
+keywordList=synonyms+antonyms+pastSynonyms+quitSynonyms+ ['to','be','hi','hello','water', 'sewerage', 'electricity', 'property','income','trade license','show','see','bills']
+keywordList=list(set(keywordList))
 
-to_be_paid=['to be paid', 'to be paid','not paid yet','not yet paid']
-
-syn=res['KeyWords'][0]['synonyms']
-
-ant=res['KeyWords'][0]['antonyms']
-
-quits=res['KeyWords'][0]['quits']
-
-syn_past=res['KeyWords'][0]['syn_past']
-
-not_paid=res['KeyWords'][0]['bigrams'][0]['values']
-for i in range(len(not_paid)):
-    not_paid[i]=res['KeyWords'][0]['bigrams'][0]['prefix']+' '+not_paid[i]
-
-not_due=res['KeyWords'][0]['bigrams'][1]['values']
-for i in range(len(not_due)):
-    not_due[i]=res['KeyWords'][0]['bigrams'][1]['prefix']+' '+not_due[i]
-
-not_to_be_paid=res['KeyWords'][0]['bigrams'][2]['values']
-for i in range(len(not_to_be_paid)):
-    not_to_be_paid[i]=res['KeyWords'][0]['bigrams'][2]['prefix']+' '+not_to_be_paid[i]
-
-
-
-
-my_list=syn+ant+syn_past+quits+ ['to','be','hi','hello','water', 'sewerage', 'electricity', 'property','income','trade license','show','see','bills']
-my_list=list(set(my_list))
-
-def rectify(s):
-    s=s.lower()
-    if s=="hi" or s=="hello":
+def rectify(sentence):
+    sentence=sentence.lower()
+    if sentence=="hi" or sentence=="hello":
         return ("hello",0)
     
-    s=s.replace('to see', 'show')
-    s=s.replace('see','show')
-    s=s.replace('to retrieve','show')
-    s=s.replace('to retrieved','show')
-    s=s.replace('to get', 'show')
-    s=s.replace('get','')
-    s=s.replace('take','')
-    s=s.replace('look','')
+    sentence=sentence.replace('to see', 'show')
+    sentence=sentence.replace('see','show')
+    sentence=sentence.replace('to retrieve','show')
+    sentence=sentence.replace('to retrieved','show')
+    sentence=sentence.replace('to get', 'show')
+    sentence=sentence.replace('get','')
+    sentence=sentence.replace('take','')
+    sentence=sentence.replace('look','')
     
     
-    s_new=""
-    for i in s:
-        if len(s_new)<2:
-            s_new=s_new+i
-        elif (s_new[len(s_new)-1]!=i or s_new[len(s_new)-2]!=i):
-            s_new=s_new+i
+    newSentence=""
+    for i in sentence:
+        if len(newSentence)<2:
+            newSentence=newSentence+i
+        elif (newSentence[len(newSentence)-1]!=i or newSentence[len(newSentence)-2]!=i):
+            newSentence=newSentence+i
 
-    s_new=s_new.replace("n't",' not')
-    s_new=s_new.replace("'d"," would")
+    newSentence=newSentence.replace("n't",' not')
+    newSentence=newSentence.replace("'d"," would")
     
-    
+    for word in newSentence.split():
+        if word in rem and word not in keep:
+            newSentence=newSentence.replace(word,'')
 
-    for i in s_new.split():
-        if i in rem and i not in keep:
-            s_new=s_new.replace(i,'')
-
-    #return spell(s_new)
-    s_new= s_new.replace('wetar', 'water')
-    s_new=s_new.replace (' want ', ' ')
-    s_new=s_new.replace(' wants ', ' ')
-    s_new=s_new.replace (' wanted ', ' ')
+   
+    newSentence=newSentence.replace('wetar', 'water')
+    newSentence=newSentence.replace (' want ', ' ')
+    newSentence=newSentence.replace(' wants ', ' ')
+    newSentence=newSentence.replace (' wanted ', ' ')
     
+    finalSentence=''
     
-    
-    
-    s_new2=''
     count=0
-    for i in s_new.split():
-        max1=0
+    for i in newSentence.split():
+        maxRatio=0
         
-        new_word=''
-        for j in my_list:
-            if fuzz.ratio(i,j)>=70 and fuzz.ratio(i,j)>max1:
+        newWord=''
+        for j in keywordList:
+            if fuzz.ratio(i,j)>=70 and fuzz.ratio(i,j)>maxRatio:
                 count+=1
-                max1=fuzz.ratio(i,j)
-                new_word=j
+                maxRatio=fuzz.ratio(i,j)
+                newWord=j
                 
-        if max1!=0:
-            s_new2+=new_word+' '
+        if maxRatio!=0:
+            finalSentence+=newWord+' '
         else:
-            s_new2+= spell(i)+' '
+            finalSentence+= spell(i)+' '
 
-    #new lines
+    
     flag=0
-    if 'show' in s_new2 or 'see' in s_new2:
+    if 'show' in finalSentence or 'see' in finalSentence:
         flag=1
-    word_list=s_new2.split();
-    s_new3=' '.join([i for i in word_list if i not in ['see','show']])
-    #newlines end
+    wordList=finalSentence.split();
+    answerSentence=' '.join([i for i in wordList if i not in ['see','show']])
+    
 
     if count!=0:
-        #return s_new2[0:len(s_new2)-1]
-        return (s_new3,flag)
+        
+        return (answerSentence,flag)
     else:
         return ("invalid",flag)
-
-#while True:
-#    a=input("type something\n")
-#    print (rectify(a)+'\n')
-        
-
-
-
