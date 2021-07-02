abaff207534c3c5c70b7dc01e346f9d05c42bf1d
Sarvesh-eGov <71336235+Sarvesh-eGov@users.noreply.github.com>
2021-07-02 11:17:49 +0530

    Code cleanup
    naming as per conventions

--- nlp-engine/src/city_audio.py
+++ nlp-engine/src/city_audio.py
@@ -9,12 +9,12 @@ from pydub import AudioSegment
 city_audio=Flask(__name__)
 run_with_ngrok(city_audio)
 
-cache_cities={}
-cache_final={}
-cache_localities={}
+cacheCities={}
+cacheFinal={}
+cacheLocalities={}
 source={}
 
-welcome= "Welcome to the *DIGIT* platform ! Please type in your city name.\n"
+welcomeMessage= "Welcome to the *DIGIT* platform ! Please type in your city name.\n"
 
 @city_audio.route('/',methods=['POST'])
 def reply():
@@ -28,13 +28,13 @@ def reply():
       'cache-control': 'no-cache'
                       }
     
-    request_data=request.get_json()
+    requestData=request.get_json()
     inp=""
     
-    if request_data["payload"]["type"]=="audio" :
-        audio_url=request_data["payload"]["payload"]["url"]
-        r=requests.get(audio_url, allow_redirects=True)
-        open('voice_message.ogg', 'wb').write(r.content)
+    if requestData["payload"]["type"]=="audio" :
+        audioUrl=requestData["payload"]["payload"]["url"]
+        getAudioFile=requests.get(audioUrl, allow_redirects=True)
+        open('voice_message.ogg', 'wb').write(getAudioFile.content)
         sound=AudioSegment.from_ogg("voice_message.ogg")
         
         sound.export("voice_message.wav", format="wav")
@@ -45,7 +45,7 @@ def reply():
             audio = r.record(source)
             inp=r.recognize_google(audio)
     else:
-        inp=request_data["payload"]["payload"]["text"]
+        inp=requestData["payload"]["payload"]["text"]
 
     
 
@@ -55,81 +55,79 @@ def reply():
     
 
     if(inp=="hi" or inp=="hello" or inp=="mseva"):
-        cache_cities.clear()
-        cache_final.clear()
-        #new
-        cache_localities.clear()
+        cacheCities.clear()
+        cacheFinal.clear()
+        
+        cacheLocalities.clear()
         k=payload.index("&message")
-        payload=payload[0:k]+"&message="+welcome+ "&src.name=LocalitySearch"
+        payload=payload[0:k]+"&message="+welcomeMessage+ "&src.name=LocalitySearch"
         
 
     elif (inp>="1" and inp<="9" and int(inp)>=1 and int(inp)<=9):
         
-        if (int(inp)>len(cache_cities) or int(inp)<1):
+        if (int(inp)>len(cacheCities) or int(inp)<1):
             k=payload.index("&message")
             payload=payload[0:k]+"&message="+"Please enter a valid selection\n"+"Type 'mseva' to return back to the main menu."+ "&src.name=LocalitySearch"
 
         else:
             
-            #new_start
-            if (len(cache_localities)!=0):
+            
+            if (len(cacheLocalities)!=0):
                 
-                #city=cache_final['city']
-                locality=cache_localities[int(inp)]
-                #m={"city":city, "locality":locality}
+                
+                locality=cacheLocalities[int(inp)]
+                
                 k=payload.index("&message")
                 payload=payload[0:k]+"&message="+"Locality detected--> "+locality.upper()+ "&src.name=LocalitySearch"
-                cache_cities.clear()
-                cache_final.clear()
-                cache_localities.clear()
+                cacheCities.clear()
+                cacheFinal.clear()
+                cacheLocalities.clear()
                 
-                #response=requests.post(url="http://0.0.0.0:8080/nlp-engine/fuzzy/locality", data=json.dumps(m), headers={"Content-Type": "application/json"})
-                #predictions= possibles=json.loads(response.text)["predictions"]
-            #new_end
+                
             else:
-                city= cache_cities[int(inp)]
+                city= cacheCities[int(inp)]
                 k=payload.index("&message")
                 payload=payload[0:k]+"&message=*"+(city[0].upper()+city[1:].lower())+"* \n"+ "Type the name of your locality\n"+"Type 'mseva' to return back to the main menu."+"&src.name=LocalitySearch"
-                cache_final["city"]=city
-                #cache_cities.clear()
+                cacheFinal["city"]=city
+                
             
 
     else:
-        if (len(cache_cities)==0):
+        if (len(cacheCities)==0):
             m={"input_city":inp,"input_lang":"hindi"}
             response=requests.post(url="http://127.0.0.1:8080/nlp-engine/fuzzy/city", data=json.dumps(m), headers={"Content-Type": "application/json"})
             cities=json.loads(response.text)["city_detected"]
             count=1
-            city_list="Did you mean any one of the following cities ? Enter the number of your city."+'\n'
+            cityList="Did you mean any one of the following cities ? Enter the number of your city."+'\n'
             
             for i in cities:
-                cache_cities[count]=i[3:]
-                city_list+=(str(count)+'.')
-                city_list+=(i[3].upper()+i[4:].lower())+'\n'
+                cacheCities[count]=i[3:]
+                cityList+=(str(count)+'.')
+                cityList+=(i[3].upper()+i[4:].lower())+'\n'
                 count+=1
 
-            city_list+='\n'
-            city_list+="Type 'mseva' to return back to the main menu."
+            cityList+='\n'
+            cityList+="Type 'mseva' to return back to the main menu."
 
             k=payload.index("&message")
-            payload=payload[0:k]+"&message="+city_list+"&src.name=LocalitySearch"
+            payload=payload[0:k]+"&message="+cityList+"&src.name=LocalitySearch"
             
-        #new_start
+       
         else:
-            m={"city":cache_final["city"],"locality":inp}
+            m={"city":cacheFinal["city"],"locality":inp}
             response=requests.post(url="http://127.0.0.1:8080/nlp-engine/fuzzy/locality", data=json.dumps(m), headers={"Content-Type": "application/json"})
             localities=json.loads(response.text)["predictions"]
             count=1
-            local_list="Did you mean any one of the following localities ? Enter the number of your locality.\n"
+            localList="Did you mean any one of the following localities ? Enter the number of your locality.\n"
 
             for i in localities:
-                cache_localities[count]=i["name"]
-                local_list+=(str(count)+'.'+i["code"]+' . '+i["name"]+'\n')
+                cacheLocalities[count]=i["name"]
+                localList+=(str(count)+'.'+i["code"]+' . '+i["name"]+'\n')
                 count+=1
 
             k=payload.index("&message")
-            payload=payload[0:k]+"&message="+local_list+"&src.name=LocalitySearch"  
-        #new_end
+            payload=payload[0:k]+"&message="+localList+"&src.name=LocalitySearch"  
+        
        
 
     payload=payload.encode('utf-8')
