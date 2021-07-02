5e4d4d5bb6d2f45529c11d25ad84fe611f7e436f
Sarvesh-eGov <71336235+Sarvesh-eGov@users.noreply.github.com>
2021-07-02 12:16:33 +0530

    [RAIN-2934] NLP Code cleanup
    Naming changed as per conventions

--- nlp-engine/src/city_gupshup.py
+++ nlp-engine/src/city_gupshup.py
@@ -11,21 +11,21 @@ translator= Translator()
 city_gupshup=Flask(__name__)
 run_with_ngrok(city_gupshup)
 
-cache_cities={}
-#cache_localities={}
-cache_final={}
+cacheCities={}
+
+cacheFinal={}
 source={}
 
 
-welcome= "Welcome to the *DIGIT* platform ! Please type in your city name.\n"
-#welcome+="Type in your queries and I will try my best to help you !\n"
+welcomeMessage= "Welcome to the *DIGIT* platform ! Please type in your city name.\n"
+
 
 @city_gupshup.route('/',methods=['POST'])
 def reply():
-    request_data=request.get_json()
-    inp=request_data["payload"]["payload"]["text"]
+    requestData=request.get_json()
+    inp=requestData["payload"]["payload"]["text"]
 
-    destination=request_data["payload"]["source"]
+    destination=requestData["payload"]["source"]
 
     default = 'channel=whatsapp&source=917834811114&destination='+destination+'&message=Please%20mention%20a%20category&src.name=LocalitySearch'
 
@@ -51,73 +51,48 @@ def reply():
       'cache-control': 'no-cache'
                       }
     if(inp=="hi" or inp=="hello" or inp=="mseva"):
-        cache_cities.clear()
-        cache_final.clear()
+        cacheCities.clear()
+        cacheFinal.clear()
         k=payload.index("&message")
-        payload=payload[0:k]+"&message="+translator.translate(welcome,dest=source["src"]).text+ "&src.name=LocalitySearch"
-        #response = requests.request("POST", url, headers=headers, data = payload)
+        payload=payload[0:k]+"&message="+translator.translate(welcomeMessage,dest=source["src"]).text+ "&src.name=LocalitySearch"
+        
 
     elif (inp>="1" and inp<="9" and int(inp)>=1 and int(inp)<=9):
-        if (int(inp)>len(cache_cities) or int(inp)<1):
+        if (int(inp)>len(cacheCities) or int(inp)<1):
             
             k=payload.index("&message")
             payload=payload[0:k]+"&message="+translator.translate("Please enter a valid selection",dest=source["src"]).text+translator.translate("Type 'mseva' to return back to the main menu.",dest=source["src"]).text+ "&src.name=LocalitySearch"
 
         else:
-            """if (len(cache_localities)!=0):
-                #city=cache_final['city']
-                locality=cache_localities[int(inp)]
-                #m={"city":city, "locality":locality}
-                k=payload.index("&message")
-                payload=payload[0:k]+"&message="+"Locality detected--> "+locality.upper()+ "&src.name=LocalitySearch"
-                cache_cities.clear()
-                cache_final.clear()
-                cache_localities.clear()
-                
-                #response=requests.post(url="http://0.0.0.0:8080/nlp-engine/fuzzy/locality", data=json.dumps(m), headers={"Content-Type": "application/json"})
-                #predictions= possibles=json.loads(response.text)["predictions"]
-            else:"""
-            city= cache_cities[int(inp)]
+           
+            city= cacheCities[int(inp)]
             k=payload.index("&message")
             payload=payload[0:k]+"&message=*"+translator.translate((city[0].upper()+city[1:].lower()),dest=source["src"]).text+"* \n"+ translator.translate("Type 'mseva' to return back to the main menu.",dest=source["src"]).text+"&src.name=LocalitySearch"
-            #cache_final["city"]=city
-            cache_cities.clear()
+            
+            cacheCities.clear()
             
 
     else:
-        if (len(cache_cities)==0):
+        if (len(cacheCities)==0):
             m={"input_city":inp,"input_lang":"hindi"}
             response=requests.post(url="http://127.0.0.1:8080/nlp-engine/fuzzy/city", data=json.dumps(m), headers={"Content-Type": "application/json"})
             cities=json.loads(response.text)["city_detected"]
             count=1
-            city_list=translator.translate("Did you mean any one of the following cities ? Enter the number of your city.",dest=source["src"]).text+'\n'
+            cityList=translator.translate("Did you mean any one of the following cities ? Enter the number of your city.",dest=source["src"]).text+'\n'
             
             for i in cities:
-                cache_cities[count]=i[3:]
-                city_list+=(str(count)+'.')
-                city_list+=translator.translate((i[3].upper()+i[4:].lower()), dest=source["src"]).text+'\n'
+                cacheCities[count]=i[3:]
+                cityList+=(str(count)+'.')
+                cityList+=translator.translate((i[3].upper()+i[4:].lower()), dest=source["src"]).text+'\n'
                 count+=1
 
-            city_list+='\n'
-            city_list+=translator.translate("Type 'mseva' to return back to the main menu.",dest=source["src"]).text
+            cityList+='\n'
+            cityList+=translator.translate("Type 'mseva' to return back to the main menu.",dest=source["src"]).text
 
             k=payload.index("&message")
-            payload=payload[0:k]+"&message="+city_list+"&src.name=LocalitySearch"
+            payload=payload[0:k]+"&message="+cityList+"&src.name=LocalitySearch"
 
-        """else:
-            m={"city":cache_final["city"],"locality":inp}
-            response=requests.post(url="http://127.0.0.1:8080/nlp-engine/fuzzy/locality", data=json.dumps(m), headers={"Content-Type": "application/json"})
-            localities=json.loads(response.text)["predictions"]
-            count=1
-            local_list="Did you mean any one of the following localities ? Enter the number of your locality.\n"
-
-            for i in localities:
-                cache_localities[count]=i["name"]
-                local_list+=(str(count)+'.'+i["code"]+' . '+i["name"]+'\n')
-                count+=1
-
-            k=payload.index("&message")
-            payload=payload[0:k]+"&message="+local_list+"&src.name=LocalitySearch"  """
+       
 
     payload=payload.encode('utf-8')
             
@@ -126,11 +101,3 @@ def reply():
     return ""
 
 city_gupshup.run()
-        
-            
-            
-
-        
-
-    
-    
