6e120d1d2047629f4f4141309e513bdcc46e14b9
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2021-07-13 10:30:28 +0530

    Rain 3039 (#842)
    * RAIN-3039 Add support for localisation
    
    * RAIN-3039 Add support for localisation
    
    * RAIN-3039 Revert the chnages for localisation

--- report/src/main/java/org/egov/report/service/IntegrationService.java
+++ report/src/main/java/org/egov/report/service/IntegrationService.java
@@ -101,7 +101,7 @@ public class IntegrationService {
                             List<Object> keysAfterLoc = new ArrayList<>();
                             List<Object> valuesAfterLoc = new ArrayList<>();
                             for (int i = 0; i < keys.size(); i++) {
-                                String servicecode = ((String) keys.get(i)).replaceAll("\\..*", "").toUpperCase();
+                                String servicecode = ((String) keys.get(i));
                                 String localisationLabel = searchColumn.getLocalisationPrefix() + servicecode;
                                 if (!valuesAfterLoc.contains(localisationLabel)) {
                                     keysAfterLoc.add(servicecode);
