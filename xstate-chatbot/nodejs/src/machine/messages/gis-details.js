76a3dbfd9d32f0c556163be9060dd943dd005f60
egovmis1 <42794309+egovmis1@users.noreply.github.com>
2021-07-15 13:05:37 +0530

    Chatbot covid punjab pmidc team gis (#843)
    * Zuul ratelimit rebase (#712)
    
    * RAIN-1854: added uuid based rate limiting
    
    * RAIN-1854: added newly limiter.properties
    
    * RAIN-1854: ^Cdated limiter.properties
    
    * RAIN-1854: changed limiter.props
    
    * updated redis host
    
    * RAIN-1854: updated limiter.props
    
    * RAIN-2282: added rate limiting on _create
    
    * Add support for form-data and not parse non-json body
    
    * Remove x-user-info from sensitive headers
    
    * EDCR-174 Enable rate limit for edcr scrutinize API
    
    EDCR-174 Enable rate limit for edcr scrutinize API
    
    * applied default policy for edcr
    
    * RAIN-2655: updated changelog
    
    * RAIN-2655: updated POM version
    
    Co-authored-by: Tarun Lalwani <tarunlalwani@gmail.com>
    Co-authored-by: vinothrallapalli-eGov <vinoth.r@egovernments.org>
    
    * Pdf async call (#714)
    
    * RAIN-2685 Added missing async keyword for function call in PDF service
    
    * RAIN-2685 Added missing async keyword for function call in PDF service
    
    * RAIN-2291 UML Diagrams for Core Services (#715)
    
    * Vul web l001 (#722)
    
    * VUL-WEB-L001: removed client secret from configuration
    
    * VUL-WEB-L001: changed host in app props
    
    * Revert "VUL-WEB-L001: changed host in app props"
    
    This reverts commit a5c53942297e490be9784586e00eaeadbd5da915.
    
    * Revert "RAIN-2291 UML Diagrams for Core Services (#715)" (#727)
    
    This reverts commit 00e40aeba44153a0130e581f5124a2c1e8681f9a.
    
    * RAIN-2655: Updated README (#729)
    
    * RAIN-2775 Upgrade pom version of report and egov-otp service (#750)
    
    * Updated Location sharing info
    
    * corrected one variable longitude
    
    * updated parameter in cova api
    
    * GIS Flow with new keyword PBGIS
    
    * removing <b> from message
    
    * removing error state
    
    * updated GIs code
    
    Co-authored-by: talele08 <talele08@users.noreply.github.com>
    Co-authored-by: Tarun Lalwani <tarunlalwani@gmail.com>
    Co-authored-by: vinothrallapalli-eGov <vinoth.r@egovernments.org>
    Co-authored-by: rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
    Co-authored-by: Sai Praneeth <72188506+praneeth-egov@users.noreply.github.com>
    Co-authored-by: Sathish P <sathish.p@egovernments.org>

---
+++ xstate-chatbot/nodejs/src/machine/messages/gis-details.js
@@ -0,0 +1,330 @@
+const messages = {
+  gisInfo: {
+    prompt: {
+      preamble: {
+        en_IN: 'What would you like to do?',
+        pa_IN: 'ਤੁਸੀਂ ਕੀ ਜਾਣਨਾ ਚਾਹੋਗੇ?',
+        hi_IN: 'आप क्या जानना चाहेंगे?',
+      },
+      options: {
+        list: ['addNewProperty', 'validateExistingProperty'],
+        messageBundle: {
+          addNewProperty: {
+            en_IN: 'Add new property',
+            pa_IN: 'ਨਵੀਂ ਜਾਇਦਾਦ ਸ਼ਾਮਲ ਕਰੋ',
+            hi_IN: 'नई संपत्ति जोड़ें',
+          },
+          validateExistingProperty: {
+            en_IN: 'Validate existing property',
+            pa_IN: 'ਮੌਜੂਦਾ ਸੰਪਤੀ ਨੂੰ ਪ੍ਰਮਾਣਿਤ ਕਰੋ',
+            hi_IN: 'मौजूदा संपत्ति सत्यापित करें',
+          },
+        },
+      },
+    },
+  },
+  propertyUseList: {
+    prompt: {
+      options: {
+        list: ['residential', 'commerical', 'residentialcommerical', 'industrial'],
+        messageBundle: {
+          residential: {
+            en_IN: 'Residential',
+            pa_IN: 'ਰਿਹਾਇਸ਼ੀ',
+            hi_IN: 'आवासीय',
+          },
+          commerical: {
+            en_IN: 'Commercial',
+            pa_IN: 'ਵਪਾਰਕ',
+            hi_IN: 'व्यावसायिक',
+          },
+          residentialcommerical: {
+            en_IN: 'Residential-Commercial',
+            pa_IN: 'ਰਿਹਾਇਸ਼ੀ-ਵਪਾਰਕ',
+            hi_IN: 'आवासीय वाणिज्यिक',
+          },
+          industrial: {
+            en_IN: 'Industrial',
+            pa_IN: 'ਉਦਯੋਗਿਕਰੋ',
+            hi_IN: 'औद्योगिक',
+          },
+        },
+      },
+    },
+  },
+  updateExistingPropertyMemu: {
+    prompt: {
+        preamble: {
+          en_IN: 'What would you like to update? ',
+          pa_IN: 'ਤੁਸੀਂ ਕੀ ਅਪਡੇਟ ਕਰਨਾ ਚਾਹੁੰਦੇ ਹੋ?  ',
+          hi_IN: 'आप क्या अपडेट करना चाहते हैं?  ',
+        },
+        options: {
+          list: ['OldHouseNo', 'blockNo', 'ownerName', 'contactNo', 'NoOfFloors', 'WaterConnection', 'SewageConnection', 'propertyId'],
+          messageBundle: {
+            OldHouseNo: {
+              en_IN: 'House Number',
+              pa_IN: 'ਘਰ ਦਾ ਨੰਬਰ',
+              hi_IN: 'घर का नंबर',
+            },
+            blockNo: {
+              en_IN: 'Block Number',
+              pa_IN: 'ਬਲਾਕ ਨੰਬਰ',
+              hi_IN: 'ब्लॉक संख्या',
+            },
+            ownerName: {
+              en_IN: 'Owner Name',
+              pa_IN: 'ਮਾਲਕ ਦਾ ਨਾਮ',
+              hi_IN: 'मालिक का नाम',
+            },
+            contactNo: {
+              en_IN: 'Contact Name',
+              pa_IN: 'ਸੰਪਰਕ ਨਾਮ',
+              hi_IN: 'संपर्क नाम',
+            },
+            NoOfFloors: {
+              en_IN: 'No Of Floors',
+              pa_IN: 'ਫਰਸ਼ਾਂ ਦੀ ਕੋਈ ਗਿਣਤੀ',
+              hi_IN: 'मंजिलों की संख्या',
+            },
+            WaterConnection: {
+              en_IN: 'Water Connection',
+              pa_IN: 'ਮੌਜੂਦਾ ਸੰਪਤੀ ਨੂੰ ਪ੍ਰਮਾਣਿਤ ਕਰੋ',
+              hi_IN: 'मौजूदा संपत्ति सत्यापित करें',
+            },
+            SewageConnection: {
+              en_IN: 'Sewage Connection',
+              pa_IN: 'ਮੌਜੂਦਾ ਸੰਪਤੀ ਨੂੰ ਪ੍ਰਮਾਣਿਤ ਕਰੋ',
+              hi_IN: 'मौजूदा संपत्ति सत्यापित करें',
+            },
+            propertyId: {
+              en_IN: 'Property Id',
+              pa_IN: 'ਮੌਜੂਦਾ ਸੰਪਤੀ ਨੂੰ ਪ੍ਰਮਾਣਿਤ ਕਰੋ',
+              hi_IN: 'मौजूदा संपत्ति सत्यापित करें',
+            },
+          },
+        },
+    },
+  },
+  selectLanguageGis: {
+    prompt: {
+      preamble: {
+        en_IN: 'Sat Sri Akal! Welcome to the GIS Chatbot.',
+      },
+      options: {
+        list: ['pa_IN', 'hi_IN', 'en_IN'],
+        messageBundle: {
+          en_IN: {
+            en_IN: 'English',
+          },
+          hi_IN: {
+            en_IN: 'हिंदी',
+          },
+          pa_IN: {
+            en_IN: 'ਪੰਜਾਬੀ',
+          },
+        },
+      },
+    },
+  },
+  invalidOption: {
+    en_IN: 'Please enter valid input',
+    pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਵੈਧ ਇੰਪੁੱਟ ਦਾਖਲ ਕਰੋ',
+    hi_IN: 'कृपया मान्य इनपुट दर्ज करें',
+  },
+  gisMobileNumber: {
+    prompt: {
+      en_IN: 'Please Enter your Mobile Number for Validation',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਪ੍ਰਮਾਣਿਕਤਾ ਲਈ ਆਪਣਾ ਮੋਬਾਈਲ ਨੰਬਰ ਦਰਜ ਕਰੋ',
+      hi_IN: 'कृपया प्रमाणीकरण के लिए अपना मोबाइल नंबर दर्ज करें',
+    },
+    error: {
+      en_IN: 'Your Entered Mobile Number is Invalid.If You are GIS officer Please enter your registered number or else press "Hi" if you are citizen',
+      pa_IN: 'ਤੁਹਾਡਾ ਦਰਜ ਕੀਤਾ ਮੋਬਾਈਲ ਨੰਬਰ ਗਲਤ ਹੈ ',
+      hi_IN: 'आपका दर्ज मोबाइल नंबर गलत है',
+    },
+  },
+  temporaryParcelId: {
+    prompt: {
+      en_IN: 'Add Temporary parcel id',
+      pa_IN: 'ਅਸਥਾਈ ਪਾਰਸਲ ਆਈਡੀ ਸ਼ਾਮਲ ਕਰੋ',
+      hi_IN: 'एक अस्थायी पार्सल आईडी जोड़ें',
+    },
+  },
+  addhouseNo: {
+    prompt: {
+      en_IN: 'Please enter your house number',
+      pa_IN: 'ਆਪਣੇ ਘਰ ਦਾ ਨੰਬਰ ਦਰਜ ਕਰੋ ਜੀ',
+      hi_IN: 'कृपया अपने घर का नंबर दर्ज करें',
+    },
+  },
+  addblockNo: {
+    prompt: {
+      en_IN: 'Please enter your block number',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਆਪਣਾ ਬਲਾਕ ਨੰਬਰ ਦਰਜ ਕਰੋ',
+      hi_IN: 'कृपया अपना ब्लॉक नंबर दर्ज करें',
+    },
+  },
+  addownerName: {
+    prompt: {
+      en_IN: 'Please enter Owner name',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਮਾਲਕ ਦਾ ਨਾਮ ਦਰਜ ਕਰੋ',
+      hi_IN: 'कृपया स्वामी का नाम दर्ज करें',
+    },
+  },
+  addcontactNo: {
+    prompt: {
+      en_IN: 'Please enter your Contact number',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਆਪਣਾ ਸੰਪਰਕ ਨੰਬਰ ਦਰਜ ਕਰੋ',
+      hi_IN: 'ब्कृपया अपना संपर्क नंबर दर्ज करें',
+    },
+    error: {
+      en_IN: 'Entered Mobile Number is incorrect',
+      pa_IN: 'ਦਰਜ ਕੀਤਾ ਮੋਬਾਈਲ ਨੰਬਰ ਗਲਤ ਹੈ',
+      hi_IN: 'दर्ज किया गया मोबाइल नंबर गलत है',
+    }
+  },
+  addpropertyuse: {
+    prompt: {
+      en_IN: 'Please enter your Property use',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੀ ਜਾਇਦਾਦ ਦੀ ਵਰਤੋਂ ਦਾਖਲ ਕਰੋ',
+      hi_IN: 'कृपया अपनी संपत्ति का उपयोग दर्ज करें',
+    },
+  },
+  addnoOfFloor: {
+    prompt: {
+      en_IN: 'Please enter Number of floors (eg. G1 with Basement ,G1+1 etc) ',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਫ਼ਰਸ਼ਾਂ ਦੀ ਗਿਣਤੀ ਦਰਜ ਕਰੋ(eg. G1 with Basement ,G1+1 etc) ',
+      hi_IN: 'कृपया मंजिलों की संख्या दर्ज करें(eg. G1 with Basement ,G1+1 etc) ',
+    },
+  },
+  addwaterconnection: {
+    prompt: {
+      en_IN: 'Please enter your Water connections number',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਆਪਣਾ ਪਾਣੀ ਦੇ ਸੰਪਰਕ ਨੰਬਰ ਭਰੋ',
+      hi_IN: 'कृपया अपना जल संपर्क नंबर दर्ज करें',
+    },
+  },
+  addsewageconnection: {
+    prompt: {
+      en_IN: 'Please enter your Sewage connection number',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਆਪਣਾ ਸੀਵਜ ਕੁਨੈਕਸ਼ਨ ਨੰਬਰ ਦਾਖਲ ਕਰੋ',
+      hi_IN: 'कृपया अपना सीवेज कनेक्शन नंबर दर्ज करें',
+    },
+  },
+  addpropertyId: {
+    prompt: {
+      en_IN: 'Please enter your Property id as per tax property',
+      pa_IN: 'ਟੈਕਸ ਜਾਇਦਾਦ ਦੇ ਅਨੁਸਾਰ ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੀ ਜਾਇਦਾਦ ID ਦਾਖਲ ਕਰੋ',
+      hi_IN: 'कृपया कर योग्य संपत्ति के अनुसार अपनी संपत्ति आईडी दर्ज करें',
+    },
+  },
+  validateExistingProperty: {
+    prompt: {
+      en_IN: 'Please Enter your parcel ID',
+      pa_IN: 'ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੀ ਪਾਰਸਲ ID ਦਿਓ',
+      hi_IN: 'ब्कृपया अपना पार्सल आईडी दर्ज करें',
+    },
+  },
+  houseNo: {
+    prompt: {
+      en_IN: '1. House No :',
+      pa_IN: '1. ਮਕਾਨ ਨੰ:',
+      hi_IN: '1. मकान नंबर:',
+    },
+  },
+  blockNo: {
+    prompt: {
+      en_IN: '2. Block No :',
+      pa_IN: '2. ਬਲਾਕ ਨੰ: ',
+      hi_IN: '2. ब्लॉक नंबर: ',
+    },
+  },
+  contactNo: {
+    prompt: {
+      en_IN: '4. Contact No :',
+      pa_IN: '4. ਸੰਪਰਕ ਨੰਬਰ: ',
+      hi_IN: '4. संपर्क फ़ोन नंबर',
+    },
+  },
+  ownerName: {
+    prompt: {
+      en_IN: '3. Owner Name :',
+      pa_IN: '3. ਮਾਲਕ ਦਾ ਨਾਮ:',
+      hi_IN: '3. मालिक का नाम:',
+    },
+  },
+  propertyUse: {
+    prompt: {
+      en_IN: '5. Property Type :',
+      pa_IN: '5. ਜਾਇਦਾਦ ਦੀ ਕਿਸਮ: ',
+      hi_IN: '5. संपत्ति के प्रकार: ',
+    },
+  },
+  noOfFloors: {
+    prompt: {
+      en_IN: '6. No of floors :',
+      pa_IN: '6. ਫ਼ਰਸ਼ਾਂ ਦੀ ਗਿਣਤੀ: ',
+      hi_IN: '6. मंजिलों की संख्या: ',
+    },
+  },
+  waterConnection: {
+    prompt: {
+      en_IN: '7. Water Connection :',
+      pa_IN: '7. ਪਾਣੀ ਦਾ ਸੰਪਰਕ: ',
+      hi_IN: '7. जल संपर्क: ',
+    },
+  },
+  sewerageConnection: {
+    prompt: {
+      en_IN: '8. Sewerage Connection :',
+      pa_IN: '8. ਸੀਵਰੇਜ ਕੁਨੈਕਸ਼ਨ: ',
+      hi_IN: '8. सीवरेज कनेक्शन: ',
+    },
+  },
+  proprtyId: {
+    prompt: {
+      en_IN: '9. Property id as per Tax property:',
+      pa_IN: '9. ਟੈਕਸ ਪ੍ਰਾਪਰਟੀ ਦੇ ਅਨੁਸਾਰ ਪ੍ਰਾਪਰਟੀ ਆਈਡੀ: ',
+      hi_IN: '9. कर संपत्ति के अनुसार संपत्ति आईडी: ',
+    },
+  },
+  anyOtherUpdate: {
+    prompt: {
+      en_IN: 'Any Other Update :',
+      pa_IN: 'ਕੋਈ ਹੋਰ ਅਪਡੇਟ:',
+      hi_IN: 'कोई अन्य अपडेट:',
+    },
+  },
+  updateProperty: {
+    prompt: {
+      en_IN: 'Your property have been updated successfully',
+      pa_IN: 'ਤੁਹਾਡੀ ਜਾਇਦਾਦ ਸ਼ਾਮਲ ਕੀਤੀ ਗਈ ਹੈ ਧੰਨਵਾਦ!',
+      hi_IN: 'आपकी संपत्ति जोड़ दी गई है। धन्यवाद!',
+    },
+  },
+  propertyAdded: {
+    prompt: {
+      en_IN: 'Your Property is Added . Thank You!',
+      pa_IN: 'ਤੁਹਾਡੀ ਜਾਇਦਾਦ ਸ਼ਾਮਲ ਕੀਤੀ ਗਈ ਹੈ ਧੰਨਵਾਦ!',
+      hi_IN: 'आपकी संपत्ति जोड़ दी गई है। धन्यवाद!',
+    },
+  },
+};
+const grammers = {
+  binaryChoice: {
+    prompt: {
+      en_IN: '\n1. Yes\n2. No',
+      pa_IN: '\n1. ਜੀ\n2. ਨਹੀਂ',
+      hi_IN: '\n1. हाँ \n2. नहीं',
+    },
+    grammer: [
+      { intention: 'YES', recognize: ['1'] },
+      { intention: 'NO', recognize: ['2'] },
+    ],
+  },
+};
+
+module.exports.messages = messages;
+module.exports.grammers = grammers;
+
---
+++ xstate-chatbot/nodejs/src/machine/service/gis-service.js
@@ -0,0 +1,144 @@
+const fetch = require('node-fetch');
+const config = require('../../env-variables');
+const FormData = require('form-data');
+
+class GisService {
+
+  async getGisDetailsFromMobileNumber(mobileNumber) {
+    const url = config.covaApiConfigs.getGISByMobileNumber;
+    var formdata = new FormData();
+    formdata.append("mobile_no", mobileNumber);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    const response = await fetch(url, requestOptions);
+    if (response.status == 200) {
+      const data = await response.json();
+      if (data.response == 1) {
+        console.log('GIS Fetch data through Mobile Number.');
+      } else {
+        console.error(`Error while fetching GIS data through Mobile Number.\nStatus: ${data.success}; Response: ${JSON.stringify(data.message)}`);
+      }
+      return data;
+    } else {
+      const responseBody = await response.json();
+      console.error(`Error while fetching GIS data through Mobile Number.\nStatus: ${data.success}; Response: ${JSON.stringify(data.message)}`);
+    }
+  }
+
+  async getPropertyFromPropertyId(propertyId) {
+
+    const url = config.covaApiConfigs.getPropertyByPropertId;
+    const formdata = new FormData();
+    formdata.append('UID', propertyId.toString());
+
+    const requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow',
+    };
+
+    const response = await fetch(url, requestOptions);
+
+    if (response.status == 200) {
+      const data = await response.json();
+
+      if (data.success == 1) {
+        console.log(`Property Details fetched succesfully : ${data.message}`);
+        return data;
+      }
+      console.log(`Property Details fetched failure : ${data.message}`);
+      return data;
+    } else {
+      const responseBody = await response.json();
+      console.error(`Property Details fetched API responded with ${JSON.stringify(responseBody)}`);
+    }
+  }
+
+  async addPropertyDetails(propertyDetails) {
+
+    const url = config.covaApiConfigs.addedNewProperty;
+    const formdata = new FormData();
+    formdata.append('UserId', propertyDetails.user_id);
+    formdata.append('ParcelID', propertyDetails.tempId);
+    formdata.append('OldHouseNo', propertyDetails.houseNo);
+    formdata.append('BlockNo', propertyDetails.blockNo);
+    formdata.append('ContactNo', propertyDetails.contactNo);
+    formdata.append('PropertyUse', propertyDetails.typeOfProperty);
+    formdata.append('NoOfFloors', propertyDetails.noOfFloors);
+    formdata.append('WaterConnection', propertyDetails.waterConnection);
+    formdata.append('SewerageConnection', propertyDetails.sewageConnection);
+    formdata.append('PropertyTax', propertyDetails.propertyId);
+    formdata.append('OwnersName', propertyDetails.ownerName);
+    const requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow',
+    };
+
+    const response = await fetch(url, requestOptions);
+
+    if (response.status == 200) {
+      const data = await response.json();
+
+      if (data.success == 1) {
+        console.log(`New Property added succesfully in property database : ${data.message}`);
+        return data;
+      }
+      console.log(`New Property addition failure : ${data.message}`);
+      return data;
+    } else {
+      const responseBody = await response.json();
+      console.error(`API responded with ${JSON.stringify(responseBody)}`);
+    }
+  }
+
+  async updatePropertyDetails(propertyDetails) {
+
+    const url = config.covaApiConfigs.updateNewProperty;
+    const formdata = new FormData();
+
+    formdata.append('UID', propertyDetails.UID);
+    formdata.append('UserId', propertyDetails.user_id);
+    formdata.append('ParcelID', propertyDetails.UID);
+    formdata.append('OldHouseNo', propertyDetails.houseNo);
+    formdata.append('BlockNo', propertyDetails.blockNo);
+    formdata.append('ContactNo', propertyDetails.contactNo);
+    formdata.append('PropertyUse', propertyDetails.typeOfProperty);
+    formdata.append('NoOfFloors', propertyDetails.noOfFloors);
+    formdata.append('WaterConnection', propertyDetails.waterConnection);
+    formdata.append('SewerageConnection', propertyDetails.sewageConnection);
+    formdata.append('PropertyTax', propertyDetails.propertyTax);
+    formdata.append('OwnersName', propertyDetails.ownerName);
+    formdata.append('add_id', propertyDetails.user_id);
+
+    const requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow',
+    };
+
+    const response = await fetch(url, requestOptions);
+
+    if (response.status == 200) {
+      const data = await response.json();
+
+      if (data.success == 1) {
+        console.log(`New Property added succesfully in property database : ${data.message}`);
+        return data;
+      }
+      console.log(`New Property addition failure : ${data.message}`);
+      return data;
+    } else {
+      const responseBody = await response.json();
+      console.error(`API responded with ${JSON.stringify(responseBody)}`);
+    }
+  }
+
+}
+
+module.exports = new GisService();
