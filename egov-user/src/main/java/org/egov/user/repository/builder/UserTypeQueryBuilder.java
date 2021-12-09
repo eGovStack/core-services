598a98fcca44232048dcfcead778f4b399ef468c
egovmis <42794267+egovmis@users.noreply.github.com>
2021-12-09 15:11:00 +0530

    Update UserTypeQueryBuilder.java

--- egov-user/src/main/java/org/egov/user/repository/builder/UserTypeQueryBuilder.java
+++ egov-user/src/main/java/org/egov/user/repository/builder/UserTypeQueryBuilder.java
@@ -145,7 +145,7 @@ public class UserTypeQueryBuilder {
         if (!userSearchCriteria.isFuzzyLogic() && userSearchCriteria.getName() != null) {
             isAppendAndClause = addAndClauseIfRequired(isAppendAndClause, selectQuery);
             selectQuery.append(" u.name like " + "'%").append(userSearchCriteria.getName().trim()).append("%'");
-            preparedStatementValues.add(userSearchCriteria.getName().trim());
+           // preparedStatementValues.add(userSearchCriteria.getName().trim());
         }
 
         if (userSearchCriteria.getActive() != null) {
