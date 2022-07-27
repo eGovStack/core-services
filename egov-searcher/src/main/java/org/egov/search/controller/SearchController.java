6a3c010e7ae8994cda4b36d1f79c6171a916b6ee
Uday Singh <95569001+uday-eGov@users.noreply.github.com>
2022-07-27 16:09:07 +0530

    Added annotation GET

--- egov-searcher/src/main/java/org/egov/search/controller/SearchController.java
+++ egov-searcher/src/main/java/org/egov/search/controller/SearchController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
