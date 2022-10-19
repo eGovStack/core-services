94dd0272d2de085f372c6a741e397a969424588a
jitendra-eGov <100871838+jitendra-eGov@users.noreply.github.com>
2022-10-19 11:00:45 +0530

    Updated import for sl4J (#1020)

--- egov-indexer/src/main/java/org/egov/infra/indexer/controller/IndexerController.java
+++ egov-indexer/src/main/java/org/egov/infra/indexer/controller/IndexerController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/index-operations")
